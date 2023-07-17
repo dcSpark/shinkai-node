26fe1e930b89834341f65e7a6bd5e6884fe26930
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-07-18 00:20:42 +0200

    Added auto header detection variant for csv parse

--- src/utils/file_parsing.rs
+++ src/utils/file_parsing.rs
@@ -3,6 +3,37 @@ use pdf_extract;
 use std::error::Error;
 use std::io::Cursor;
 
+/// Parse CSV data from a buffer and attempt to automatically detect headers.
+///
+/// # Arguments
+///
+/// * `buffer` - A byte slice containing the CSV data.
+///
+/// # Returns
+///
+/// A `Result` containing a `Vec<Vec<String>>`. Each inner `Vec<String>>`
+/// represents a row in the CSV, and contains the column values for that row. If
+/// an error occurs while parsing the CSV data, the `Result` will contain an
+/// `Error`.
+fn parse_csv_auto(buffer: &[u8]) -> Result<Vec<Vec<String>>, Box<dyn Error>> {
+    let mut reader = Reader::from_reader(Cursor::new(buffer));
+    let headers = reader.headers()?.iter().map(String::from).collect::<Vec<String>>();
+
+    // Heuristics for header detection
+    let likely_header = headers.iter().all(|s| {
+        // Check for all alphabetical characters
+        let is_alphabetic = s.chars().all(|c| c.is_alphabetic() || c.is_whitespace());
+        // Check for duplicates
+        let no_duplicates = headers.iter().filter(|&item| item == s).count() == 1;
+        // Check for prohibited characters
+        let no_prohibited_chars = !s.contains(&['@', '#', '$', '%', '^', '&', '*'][..]);
+
+        is_alphabetic && no_duplicates && no_prohibited_chars
+    });
+
+    parse_csv(&buffer, likely_header)
+}
+
 /// Parse CSV data from a buffer.
 ///
 /// # Arguments
