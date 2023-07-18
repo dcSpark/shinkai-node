b7b1e65762d5021b58500c9d1bd9eb2e7f7bac16
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-07-18 22:54:54 +0200

    Fixed embedding download build script

--- build.rs
+++ build.rs
@@ -1,33 +1,33 @@
 // build.rs
-use std::fs;
-use std::io::Write;
+use std::fs::File;
+use std::io::copy;
 use std::path::Path;
-use std::process::Command;
 
 fn main() {
     prost_build::compile_protos(&["protos/shinkai_message_proto.proto"], &["protos"]).unwrap();
 
-    /// Auto-fetching embeddings model if not found in local folder
     let filename = "pythia-160m-q4_0.bin";
     let url = "https://huggingface.co/rustformers/pythia-ggml/resolve/main/pythia-160m-q4_0.bin";
 
     // Check if the file exists
     if !Path::new(filename).exists() {
         // File does not exist, download it
-        println!("Downloading Pythia Embeddings Model...");
-        let output = Command::new("curl")
-            .arg("-o")
-            .arg(filename)
-            .arg(url)
-            .output()
-            .expect("Failed to download the model.");
+        println!("Downloading Embeddings Model...");
 
-        // Check the download status
-        if output.status.success() {
-            println!("Model downloaded successfully.");
-        } else {
-            let error_message = String::from_utf8_lossy(&output.stderr);
-            println!("Failed to download the model: {}", error_message);
+        let response = reqwest::blocking::get(url);
+        match response {
+            Ok(mut resp) => {
+                if resp.status().is_success() {
+                    let mut out = File::create(filename).expect("Failed to create file");
+                    copy(&mut resp, &mut out).expect("Failed to copy content");
+                    println!("Model downloaded successfully.");
+                } else {
+                    println!("Failed to download the model: {}", resp.status());
+                }
+            }
+            Err(e) => {
+                println!("Failed to download the model: {}", e);
+            }
         }
     } else {
         println!("File already exists.");
