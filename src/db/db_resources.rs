859304794f4ab0859800cbbcb1761c50a030bc48
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-07-25 21:17:29 +0200

    Implemented resource sim search from db

--- src/db/db_resources.rs
+++ src/db/db_resources.rs
@@ -3,6 +3,7 @@ use crate::resources::document::DocumentResource;
 use crate::resources::embeddings::Embedding;
 use crate::resources::resource::RetrievedDataChunk;
 use crate::resources::resource::{Resource, ResourceType};
+use crate::resources::resource_errors::ResourceError;
 use crate::resources::router::ResourceRouter;
 use rocksdb::{ColumnFamilyDescriptor, Error, IteratorMode, Options, DB};
 use serde_json::{from_str, to_string};
@@ -72,8 +73,8 @@ impl ShinkaiDB {
     }
 
     /// Fetches the Resource from the DB using the provided key
-    /// in the resources topic, and parses it into a DocumentResource
-    ///
+    /// in the resources topic, and attempts to parse it into
+    /// the specified ResourceType
     pub fn get_resource<K: AsRef<[u8]>>(
         &self,
         key: K,
@@ -88,13 +89,12 @@ impl ShinkaiDB {
             let document_resource: DocumentResource = from_str(json_str)?;
             Ok(Box::new(document_resource))
         } else {
-            let document_resource: DocumentResource = from_str(json_str)?;
-            Ok(Box::new(document_resource))
+            Err(ShinkaiDBError::from(ResourceError::InvalidResourceType))
         }
     }
 
     /// Fetches the Resource Router from the `resource_router` key
-    /// in the resources topic, and parses it into a ResourceRouter
+    /// in the resources topic, and attempts to parse it into a ResourceRouter
     pub fn get_resource_router(&self) -> Result<ResourceRouter, ShinkaiDBError> {
         // Fetch and convert the bytes to a valid UTF-8 string
         let bytes = self.get_cf(Topic::Resources, ResourceRouter::db_key())?;
@@ -106,54 +106,54 @@ impl ShinkaiDB {
         Ok(router)
     }
 
-    // /// Performs a 2-tier vector similarity search using a query embedding across all resources.
-    // /// The search first finds the most similar resources based on their resource_embedding
-    // /// and takes the num_of_resources amount of resources.
-    // ///
-    // /// From there a similarity search is performed on each resource with the query embedding,
-    // /// and the results from all resources are then collected, sorted, and the top num_of_results
-    // /// RetriedDataChunks based on similarity score are returned.
-    // ///
-    // /// # Arguments
-    // ///
-    // /// * `query` - An embedding that is the basis for the similarity search.
-    // /// * `num_of_resources` - The number of most similar resources to perform
-    // ///   similarity searches inside of. Increasing this improves search quality, but makes it slower.
-    // /// * `num_of_results` - The number of top results to return (top-k)
-    // ///
-    // /// # Returns
-    // ///
-    // /// A `vector of `RetrievedDataChunk`s, potentially from multiple resources
-    // /// sorted by similarity score in descending order.
-    // pub fn similarity_search(
-    //     &self,
-    //     query: Embedding,
-    //     num_of_resources: u64,
-    //     num_of_results: u64,
-    // ) -> Result<Vec<RetrievedDataChunk>, ShinkaiDBError> {
-    //     Ok(vec![])
-    // }
+    /// Performs a 2-tier vector similarity search using a query embedding across all resources.
+    /// The search first finds the most similar resources based on their resource_embedding
+    /// and takes the num_of_resources amount of resources.
+    ///
+    /// From there a similarity search is performed on each resource with the query embedding,
+    /// and the results from all resources are then collected, sorted, and the top num_of_results
+    /// RetriedDataChunks based on similarity score are returned.
+    ///
+    /// # Arguments
+    ///
+    /// * `query` - An embedding that is the basis for the similarity search.
+    /// * `num_of_resources` - The number of most similar resources to perform
+    ///   similarity searches inside of. Increasing this improves search quality, but makes it slower.
+    /// * `num_of_results` - The number of top results to return (top-k)
+    ///
+    /// # Returns
+    ///
+    /// A `vector of `RetrievedDataChunk`s, potentially from multiple resources
+    /// sorted by similarity score in descending order.
+    pub fn similarity_search(
+        &self,
+        query: Embedding,
+        num_of_resources: u64,
+        num_of_results: u64,
+    ) -> Result<Vec<RetrievedDataChunk>, ShinkaiDBError> {
+        Ok(vec![])
+    }
 
-    // /// Performs a vector similarity search using a query embedding and returns the
-    // /// num_of_resources amount of most similar Resources
-    // ///
-    // /// # Arguments
-    // ///
-    // /// * `query` - An embedding that is the basis for the similarity search.
-    // /// * `num_of_resources` - The number of most similar resources to perform
-    // pub fn similarity_search_resources(
-    //     &self,
-    //     query: Embedding,
-    //     num_of_resources: u64,
-    // ) -> Result<Vec<Box<dyn Resource>>, ShinkaiDBError> {
-    //     let router = self.get_resource_router()?;
-    //     let resource_pointers = router.similarity_search(query, num_of_resources)?;
+    /// Performs a vector similarity search using a query embedding and returns the
+    /// num_of_resources amount of most similar Resources.
+    ///
+    /// # Arguments
+    ///
+    /// * `query` - An embedding that is the basis for the similarity search.
+    /// * `num_of_resources` - The number of most similar resources to perform
+    pub fn similarity_search_resources(
+        &self,
+        query: Embedding,
+        num_of_resources: u64,
+    ) -> Result<Vec<Box<dyn Resource>>, ShinkaiDBError> {
+        let router = self.get_resource_router()?;
+        let resource_pointers = router.similarity_search(query, num_of_resources);
 
-    //     let mut resources = vec![];
-    //     for key in resources_db_keys {
-    //         resources.push(self.get_resource(key)?);
-    //     }
+        let mut resources = vec![];
+        for res_pointer in resource_pointers {
+            resources.push(self.get_resource(res_pointer.db_key, &(res_pointer.resource_type))?);
+        }
 
-    //     Ok(vec![])
-    // }
+        Ok(resources)
+    }
 }
