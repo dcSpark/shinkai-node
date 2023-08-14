78645326ff2e1c67eee51b28ee0b7a5990759dd9
Grzegorz Drozda <grzegorz@dcspark.io>
2023-08-14 08:01:31 +0000

    Branch units

--- .github/run-app-unit-tests.sh
+++ .github/run-app-unit-tests.sh
@@ -4,8 +4,7 @@ NVM_DIR=/usr/local/nvm
 NODE_VERSION=v16.20.1
 
 PATH=$PATH:$NVM_DIR/versions/node/$NODE_VERSION/bin:$PATH
-RUN node -v 
-
+node -v 
 
 cd /app/shinkai-app
 source $NVM_DIR/nvm.sh && nvm use
