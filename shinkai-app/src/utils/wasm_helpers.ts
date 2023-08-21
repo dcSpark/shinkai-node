2cc0cb86e2386c572dd891bf388c6bf037584b6f
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-08-21 12:05:39 -0500

    fix test

--- shinkai-app/src/utils/wasm_helpers.ts
+++ shinkai-app/src/utils/wasm_helpers.ts
@@ -4,7 +4,7 @@ import * as ed from '@noble/ed25519';
 
 type HexString = string;
 
-function toHexString(byteArray: Uint8Array) {
+export function toHexString(byteArray: Uint8Array) {
   return Array.from(byteArray, function(byte) {
     return ('0' + (byte & 0xFF).toString(16)).slice(-2);
   }).join('')
