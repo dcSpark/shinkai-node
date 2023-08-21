0b13a0406492a553893acc09ebfba6a565d6d614
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-08-21 08:04:44 -0500

    update shinkai-app to use hex rather than bs58

--- shinkai-app/src/utils/wasm_helpers.ts
+++ shinkai-app/src/utils/wasm_helpers.ts
@@ -1,14 +1,19 @@
 
 import { generateKeyPair } from 'curve25519-js';
 import * as ed from '@noble/ed25519';
-import bs58 from 'bs58';
 
-type Base58String = string;
+type HexString = string;
 
-export const generateEncryptionKeys = async (seed: Uint8Array): Promise<{my_encryption_sk_string: Base58String, my_encryption_pk_string: Base58String}> => {
+function toHexString(byteArray: Uint8Array) {
+  return Array.from(byteArray, function(byte) {
+    return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+  }).join('')
+}
+
+export const generateEncryptionKeys = async (seed: Uint8Array): Promise<{my_encryption_sk_string: HexString, my_encryption_pk_string: HexString}> => {
   const encryptionKeys = generateKeyPair(seed);
-  const my_encryption_sk_string: Base58String = bs58.encode(new Uint8Array(encryptionKeys.private));
-  const my_encryption_pk_string: Base58String = bs58.encode(new Uint8Array(encryptionKeys.public));
+  const my_encryption_sk_string: string = toHexString(encryptionKeys.private);
+  const my_encryption_pk_string: string = toHexString(encryptionKeys.public);
 
   return {
     my_encryption_sk_string,
@@ -16,12 +21,12 @@ export const generateEncryptionKeys = async (seed: Uint8Array): Promise<{my_encr
   }
 }
 
-export const generateSignatureKeys = async (): Promise<{my_identity_sk_string: Base58String, my_identity_pk_string: Base58String}> => {
-  const privKey = ed.utils.randomPrivateKey(); // Secure random private key
+export const generateSignatureKeys = async (): Promise<{my_identity_sk_string: HexString, my_identity_pk_string: HexString}> => {
+  const privKey = ed.utils.randomPrivateKey();
   const pubKey = await ed.getPublicKeyAsync(privKey);
 
-  const my_identity_sk_string: Base58String = bs58.encode(new Uint8Array(privKey));
-  const my_identity_pk_string: Base58String = bs58.encode(new Uint8Array(pubKey));
+  const my_identity_sk_string: string = toHexString(privKey);
+  const my_identity_pk_string: string = toHexString(pubKey);
 
   return {
     my_identity_sk_string,
@@ -29,7 +34,7 @@ export const generateSignatureKeys = async (): Promise<{my_identity_sk_string: B
   }
 }
 
-export const test_util_generateKeys = async (): Promise<{my_encryption_sk_string: Base58String, my_encryption_pk_string: Base58String, receiver_public_key_string: Base58String, my_identity_sk_string: Base58String, my_identity_pk_string: Base58String}> => {
+export const test_util_generateKeys = async (): Promise<{my_encryption_sk_string: HexString, my_encryption_pk_string: HexString, receiver_public_key_string: HexString, my_identity_sk_string: HexString, my_identity_pk_string: HexString}> => {
   const seed = new Uint8Array(32);
 
   const encryptionKeys = await generateEncryptionKeys(seed);
