19c8df562b5f867b5e9faf1306584709fd598a3d
Edward A <ac.edward@gmail.com>
2023-08-31 07:44:50 -0400

    added throw error

--- typescript/@shinkai/toolkit-executor/test/validate.test.js
+++ typescript/@shinkai/toolkit-executor/test/validate.test.js
@@ -3,6 +3,28 @@ const fs = require('fs');
 const {validate} = require('./../build/exec-mode');
 
 process.env.LOG = 'false';
+/** 
+ * Toolkit validation used.
+ * 
+  toolkitHeaders = [
+    {
+      name: 'example',
+      description: 'Example Header',
+      type: DATA_TYPES.BOOLEAN,
+    },
+  ];
+
+  validateHeaders(headers: Record<string, string>): Promise<boolean> {
+    console.log(headers);
+    if (String(headers['example']) === String(false)) {
+      throw new Error('Invalid value for example header');
+    }
+    if (String(headers['example']) === String(true)) {
+      return Promise.resolve(true);
+    }
+    return Promise.resolve(false);
+  }
+ */
 
 describe('Test Runner: Validate', () => {
   test('Validate negative', async () => {
@@ -21,5 +43,11 @@ describe('Test Runner: Validate', () => {
     const data = JSON.parse(fs.readFileSync(__dirname + '/data/header.json', 'utf8'));
     const x = await validate(data.source, JSON.stringify({ 'x-shinkai-example': '1' }));
     expect(x).toEqual({"result": {"error": "ValidationError: \"example\" must be a boolean" } });
+  });
+
+  test('Throw error', async () => {
+    const data = JSON.parse(fs.readFileSync(__dirname + '/data/header.json', 'utf8'));
+    const x = await validate(data.source, JSON.stringify({ 'x-shinkai-example': 'false' }));
+    expect(x).toEqual({"result": {"error": "Invalid value for example header" } });
   });
 });
