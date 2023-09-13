b56d570e73ce527e1208842e0b02ce1b12d5ceca
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-09-13 22:51:57 +0200

    Specced out job step plan/execution flow

--- src/utils/job_prompts.rs
+++ src/utils/job_prompts.rs
@@ -1,16 +1,89 @@
 use lazy_static::lazy_static;
 
+//
+// Core Job Step Flow
+//
+// Note this will all happen within a single Job step. We will probably end up summarizing the context/results from previous steps into the step history to be included as the base initial context for new steps.
+//
+// 0. User submits an initial message/request to their AI Agent.
+// 1. An initial bootstrap plan is created based on the initial request from the user.
+//
+// 2. We enter into "analysis phase".
+// 3a. Iterating starting from the first point in the plan, we ask the LLM true/false if it can provide an answer given it's personal knowledge + current context.
+// 3b. If it can then we mark this analysis step as "prepared" and go back to 3a for the next bootstrap plan task.
+// 3c. If not we tell the LLM to search for tools that would work for this task.
+// 4a. We return a list of tools to it, and ask it to either select one, or return an error message.
+// 4b. If it returns an error message, it means the plan can not be completed/Agent has failed, and we exit/ send message back to user with the error message.
+// 4c. If it chooses one, we fetch the tool info including the input EBNF.
+// 5a. We now show the input EBNF to the LLM, and ask it whether or not it has all the needed knowledge + potential data in the current context to be able to use the tool. (In either case  after the LLM chooses)
+// 5b. The LLM says it has all the needed info, then we add the tool's name/input EBNF to the current context, and either go back to 3a for the next bootstrap plan task if the task is now finished/prepared, or go to 6 if this tool was searched for to find an input for another tool.
+// 5c. The LLM doesn't have all the info it needs, so it performs another tool search and we go back to 4a.
+// 6. After resolving 4-5 for the new tool search, the new tool's input EBNF has been added into the context window, which will allow us to go back to 5a for the original tool, which enables the LLM to now state it has all the info it needs (marking the analysis step as prepared), thus going back to 3a for the next top level task.
+// 7. After iterating through all the bootstrap plan tasks and analyzing them, we have created an "execution plan" that specifies all tool calls which will need to be made.
+//
+// 8. We now move to the "execution phase".
+// 9. Using the execution plan, we move forward alternating between inferencing the LLM and executing a tool, as dictated by the plan.
+// 10. To start we inference the LLM with the user's initial request text + the input EBNF of the first tool, and tell the LLM to fill out the input EBNF with real data.
+// 11. The input JSON is taken and the tool is called/executed, with the results being added into the context.
+// 12. With the tool executed, we now inference the LLM with just the context + the input EBNF of the next tool that it needs to fill out (we can skip user's request text).
+// 13. We iterate through the entire execution plan (looping back/forth between 11/12) and arrive at the end with a context filled with all relevant data needed to answer the user's initial request.
+// 14. We inference the LLM one last time, providing it just the context + list of executed tools, and telling it to respond to the user's request by using/summarizing the results.
+//
+//
+//
+//
+
 lazy_static! {
-    static ref task_bootstrap_prompt: String = String::from(
+    static ref top_level_plan_bootstrap_prompt: String = String::from(
         r#"
-    You are an assistant running in a system who only has access to a series of tools and your own knowledge. The user has asked you:
+
+    You are an assistant running in a system who only has access to a series of tools and your own knowledge to accomplish any task.
+
+ The user has asked the system:
 
     `What is the weather like today in New York?`
+
+Create a plan that the system will need to take in order to fulfill the user's task. Make sure to make separate steps for any sub-task where data, computation, or API access may need to happen from different sources.
+
+Keep each step in the plan extremely concise/high level comprising of a single sentence each. Do not mention anything optional, nothing about error checking or logging or displaying data. Anything related to parsing/formatting can be merged together into a single step. Any calls to APIs, including parsing the resulting data from the API, should be considered as a single step.
+
+Respond using the following EBNF and absolutely nothing else:
+"{" "plan" ":" "[" string ("," string)* "]" "}"
+
+"#
+    );
+
+
+// Output:
+// {
+// "plan": [
+// "Retrieve the current date and time for New York.",
+// "Query a weather API for New York's current weather using the obtained date and time.",
+// "Parse the weather data to extract the current weather conditions."
+// ]
+// }
+
+
+
+// Example ebnf of weather fetch output for testing
+// weather-fetch-output ::= "{" "city" ":" text "," "weather-description" ":" text "," "tool" ": \"Weather Fetch\", "}"  text ::= [a-zA-Z0-9_]+
+
+    static ref task_bootstrap_prompt: String = String::from(
+        r#"
+    You are an assistant running in a system who only has access to a series of tools, your own knowledge, and the current context of acquired info includes:
+
+    ```
+    Datetime ::= 4DIGIT "-" 2DIGIT "-" 2DIGIT "T" 2DIGIT ":" 2DIGIT ":" 2DIGIT
+    ```
+
+    The current task at hand is:
+
+    `Query a weather API for New York's current weather using the obtained date and time.`
 
     
     If it is a task not pertaining to recent/current knowledge and you can respond respond directly without any external help, respond using the following EBNF and absolutely nothing else:
 
-    `"{" "answer" ":" string "}"`
+    `"{" "prepared" ":" true "}"`
 
     If you do not have the ability to respond correctly yourself, it is your goal is to find the final tool that will provide you with the capabilities you need. 
     Search to find tools which you can use, respond using the following EBNF and absolutely nothing else:
@@ -25,9 +98,15 @@ lazy_static! {
     static ref tool_selection_prompt: String = String::from(
         r#"
 
-    You are an assistant running in a system who only has access to a series of tools and your own knowledge. The user has asked the system:
+    You are an assistant running in a system who only has access to a series of tools, your own knowledge, and the current context of acquired info includes:
 
-    `What is the weather like today in New York?`
+    ```
+    Datetime: 2023-09-13T14:30:00
+    ```
+
+    The current task at hand is:
+
+    `Query a weather API for New York's current weather using the obtained date and time.`
 
     Here are up to 10 of the most relevant tools available:
     1. Name: Weather Fetch - Description: Requests weather via an API given a city name.
@@ -53,18 +132,24 @@ lazy_static! {
     static ref tool_ebnf_prompt: String = String::from(
         r#"
 
-    You are an assistant running in a system who only has access to a series of tools and your own knowledge. The user has asked the system:
+    You are an assistant running in a system who only has access to a series of tools, your own knowledge, and the current context of acquired info includes:
 
-    `What is the weather like today in New York?`
+    ```
+    Datetime: 2023-09-13T14:30:00
+    ```
+
+    The current task at hand is:
+
+    `Query a weather API for New York's current weather using the obtained date and time.`
 
     The system has selected the following tool to be used:
 
-    Name: Weather Fetch
+    Tool Name: Weather Fetch
+    Toolkit Name: weather-toolkit
     Description: Requests weather via an API given a city name.
-    Tool Input EBNF: ...
-    Tool Output EBNF: ...
+    Tool Input EBNF: "{" "city" ":" text "," "datetime" ":" text "," "tool" ": \"Weather Fetch\"," "toolkit" ": \"weather-toolkit\" }"  text ::= [a-zA-Z0-9_]+ 
 
-    Your goal is to decide whether you have all of the information you need to fill out the Tool Input EBNF.
+    Your goal is to decide whether for each field in the Tool Input EBNF, you have been provided all the needed data to fill it out fully.
 
     If all of the data/information to use the tool is available, respond using the following EBNF and absolutely nothing else:
 
@@ -78,43 +163,5 @@ lazy_static! {
 
 
     "#
-    );
-    pub static ref JOB_INIT_PROMPT: String = String::from(
-        r#"You are an agent who is currently running a job which receives task requests as messages, and outputs new messages that have tool calls which will be executed. When you respond, you must specify a list of one or more messages, with only the messages returned, no explanation or any other text. But make sure to explain in the content of the message the included results of the tools, and include the sub variable like `$1`.
-
-Note, you can send a message back to yourself recursively by inputting the job id in the userid field. Use this when you need to use two tools consecutively.
-
-Here is a grammar you must respond with and nothing else:
-
-
-root ::= message [<U+000A>] root | message
-message ::= '{' '"' 'to' '"' ':' '"' userid '"' ',' '"' 'content' '"' ':' '"' content_var '"' ',' '"' 'tool-calls' '"' ':' '[' tool_call_list ']' '}'
-userid ::= '[@][@][a-zA-Z0-9._]+[.shinkai]'
-content_var ::= content_text tool_output content_text
-content_text ::= string
-char ::= string
-tool_output ::= '$' tool_id
-tool_call_list ::= tool_call ',' tool_call_list | tool_call
-tool_call ::= '"' tool_id '"' ':' '{' tool_specific '}'
-tool_id ::= number
-tool_specific ::= tool_1 | tool_2 | tool_3
-tool_1 ::= '{' '"' 'Weather' '"' ':' '"' city '"' '}'
-tool_2 ::= '{' '"' 'DateTime' '"' ':' '"' string '"' '}'
-tool_3 ::= '{' '"' 'Vector Search' '"' ':' '"' query '"' '}'
-city ::= string
-query ::= string
-string ::= '[a-zA-Z0-9 ,.?!_]*'
-number ::= '[1-9][0-9]*'
-
-My user id: @@bob.shinkai
-Job id: 2as23gas3y68aje
-
-Task:
-
-- fetch the time and weather in Vancouver, sending it back to me
-- fetch the time and weather in Vancouver, sending it back to my friend @@alice.shinkai
-- search my vector database for "My Home Town" to find where my home town was, and fetch the weather for it after
-
-```json"#
     );
 }
