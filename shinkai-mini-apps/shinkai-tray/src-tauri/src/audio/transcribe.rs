619342ad9b9bc01681f4410e0a3dab2e8ed5ec4b
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-09-20 17:08:40 -0500

    improve voice commands

--- shinkai-mini-apps/shinkai-tray/src-tauri/src/audio/transcribe.rs
+++ shinkai-mini-apps/shinkai-tray/src-tauri/src/audio/transcribe.rs
@@ -1,7 +1,10 @@
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::BackendSpecificError;
+use std::convert::TryInto;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::{Duration, Instant};
+use webrtc_vad::Vad;
 use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
 
 pub fn run<T>(
@@ -26,55 +29,95 @@ pub fn run<T>(
     // Create a buffer to accumulate audio data
     let buffer = Arc::new(Mutex::new(Vec::new()));
 
+    // Initialize the VAD
+    let vad = Arc::new(Mutex::new(Vad::new(config.sample_rate.0.try_into().unwrap()).unwrap()));
+    vad.lock()
+        .unwrap()
+        .fvad_set_mode(webrtc_vad::VadMode::VeryAggressive)
+        .unwrap();
+
+    println!("Selected input device: {}", device.name().unwrap());
+    let mut start_time = None;
+    let mut last_voice_activity = None;
+
     // Normal Code
     let stream = device
         .build_input_stream(
             &config_clone,
             move |data: &[T], _: &cpal::InputCallbackInfo| {
-                let is_activated = is_activated.lock().unwrap();
-                if *is_activated {
-                    // Convert the audio data to f32 and add it to the buffer
-                    let mut buffer = buffer.lock().unwrap();
-                    buffer.extend(data.iter().map(|sample| (*sample).into()));
+                // Convert the incoming audio data to f32 and add it to the buffer
+                let mut buffer = buffer.lock().unwrap();
+                buffer.extend(data.iter().map(|sample| (*sample).into()));
 
-                    // If the buffer has more than 2-3 seconds of audio data, process it
-                    if buffer.len() >= (4 * config.sample_rate.0 as usize) {
-                        let buffer_clone = buffer.clone();
-                        for sample in buffer_clone {
-                            let sample_f32: f32 = sample.into();
-                            writer.write_sample(sample_f32).unwrap();
+                let mut is_activated = is_activated.lock().unwrap();
+                let mut vad = Vad::new(config.sample_rate.0.try_into().unwrap()).unwrap();
+                vad.fvad_set_mode(webrtc_vad::VadMode::VeryAggressive).unwrap();
+
+                // print buffer length
+                // println!("Buffer length: {}", buffer.len());
+                if !buffer.is_empty() && buffer.len() >= 480 {
+                    // println!("Buffer length: {}", buffer.len());
+                    let buffer_i16: Vec<i16> = buffer.iter().map(|&f| (f * i16::MAX as f32) as i16).collect();
+                    let is_voice = vad.is_voice_segment(&buffer_i16[(buffer.len() - 480)..]).unwrap();
+                    // println!("Is voice: {}", is_voice);
+
+                    if is_voice {
+                        last_voice_activity = Some(Instant::now());
+                        if !*is_activated {
+                            // Start recording
+                            *is_activated = true;
+                            start_time = Some(Instant::now());
+                            println!("Started recording");
                         }
-                        let ctx = ctx.lock().unwrap();
-                        let mut state = ctx.create_state().expect("failed to create state");
+                    } else if let Some(last_voice_activity) = last_voice_activity {
+                        if last_voice_activity.elapsed() > Duration::from_secs_f32(1.0) && *is_activated {
+                            // Stop recording
+                            *is_activated = false;
+                            if let Some(start_time) = start_time {
+                                let duration = start_time.elapsed();
+                                println!("Stopped recording after {} seconds", duration.as_secs());
 
-                        let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
-                        params.set_n_threads(2);
-                        // params.set_translate(true);
-                        params.set_print_special(false);
-                        params.set_print_progress(false);
-                        params.set_print_realtime(false);
-                        params.set_print_timestamps(false);
+                                // Process the audio data here
+                                let buffer_clone = buffer.clone();
+                                for sample in buffer_clone {
+                                    let sample_f32: f32 = sample.into();
+                                    writer.write_sample(sample_f32).unwrap();
+                                }
+                                let ctx = ctx.lock().unwrap();
+                                let mut state = ctx.create_state().expect("failed to create state");
 
-                        let audio_data = whisper_rs::convert_stereo_to_mono_audio(&buffer).unwrap();
+                                let mut params = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
+                                params.set_n_threads(2);
+                                params.set_print_special(false);
+                                params.set_print_progress(false);
+                                params.set_print_realtime(false);
+                                params.set_print_timestamps(false);
 
-                        state.full(params, &audio_data).expect("failed to run model");
+                                let audio_data = whisper_rs::convert_stereo_to_mono_audio(&buffer).unwrap();
 
-                        let num_segments = state.full_n_segments().expect("failed to get number of segments");
-                        for i in 0..num_segments {
-                            let segment = state.full_get_segment_text(i).expect("failed to get segment");
-                            println!("Segment {}: {}", i, segment);
+                                state.full(params, &audio_data).expect("failed to run model");
 
-                            let start_timestamp = state
-                                .full_get_segment_t0(i)
-                                .expect("failed to get segment start timestamp");
-                            let end_timestamp = state
-                                .full_get_segment_t1(i)
-                                .expect("failed to get segment end timestamp");
-                            println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
+                                let num_segments = state.full_n_segments().expect("failed to get number of segments");
+                                for i in 0..num_segments {
+                                    let segment = state.full_get_segment_text(i).expect("failed to get segment");
+                                    println!("Segment {}: {}", i, segment);
+
+                                    let start_timestamp = state
+                                        .full_get_segment_t0(i)
+                                        .expect("failed to get segment start timestamp");
+                                    let end_timestamp = state
+                                        .full_get_segment_t1(i)
+                                        .expect("failed to get segment end timestamp");
+                                    println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
+                                }
+
+                                // Clear the buffer
+                                buffer.clear();
+
+                                // Reset the is_activated flag
+                                *is_activated = false;
+                            }
                         }
-
-                        // Clear the buffer
-                        buffer.clear();
                     }
                 }
             },
