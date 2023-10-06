2542bc98315e17d1ba5d26587f89eac2349dd0fb
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-10-06 18:39:58 +0200

    Reworked UnstructuredElement type

--- shinkai-libs/shinkai-vector-resources/src/unstructured.rs
+++ shinkai-libs/shinkai-vector-resources/src/unstructured.rs
@@ -1,4 +1,12 @@
+use crate::base_vector_resources::BaseVectorResource;
+use crate::data_tags::DataTag;
+use crate::document_resource::DocumentVectorResource;
+use crate::embedding_generator::EmbeddingGenerator;
 use crate::resource_errors::VectorResourceError;
+use crate::source::VRSource;
+use crate::vector_resource::VectorResource;
+use blake3::Hasher;
+use keyphrases::KeyPhraseExtractor;
 use reqwest::blocking::multipart as blocking_multipart;
 use reqwest::multipart;
 use serde::Deserialize;
@@ -7,18 +15,46 @@ use serde_json::Value as JsonValue;
 #[derive(Debug)]
 pub struct UnstructuredAPI {
     api_url: String,
+    api_key: Option<String>,
 }
 
 impl UnstructuredAPI {
-    pub fn new(api_url: String) -> Self {
-        Self { api_url }
+    pub fn new(api_url: String, api_key: Option<String>) -> Self {
+        Self { api_url, api_key }
     }
 
+    // /// Makes a blocking request to process a file in a buffer to Unstructured,
+    // /// and then processing the returned results into a BaseVectorResource
+    // pub fn process_file_blocking(
+    //     &self,
+    //     file_buffer: Vec<u8>,
+    //     generator: &dyn EmbeddingGenerator,
+    //     name: &str,
+    //     desc: Option<&str>,
+    //     source: VRSource,
+    //     parsing_tags: &Vec<DataTag>,
+    //     max_chunk_size: u64,
+    // ) -> Result<BaseVectorResource, VectorResourceError> {
+    //     // Parse pdf into groups of lines + a resource_id from the hash of the data
+    //     let resource_id = UnstructuredParser::generate_data_hash(&file_buffer);
+    //     let elements = self.process_file_request_blocking(file_buffer, name)?;
+    //     eprintln!("Parsed file composed of {} elements", elements.len());
+
+    //     // Create doc resource and initial setup
+    //     let mut doc = DocumentVectorResource::new_empty(name, desc, source, &resource_id);
+    //     doc.set_embedding_model_used(generator.model_type());
+
+    //     // Extract keywords from the elements
+    //     let keywords = UnstructuredParser::extract_keywords(elements, 50);
+    // }
+
+    /// Makes a blocking request to process a file in a buffer into a list of
+    /// UnstructuredElements
     pub fn process_file_request_blocking(
         &self,
         file_buffer: Vec<u8>,
         file_name: &str,
-    ) -> Result<JsonValue, VectorResourceError> {
+    ) -> Result<Vec<UnstructuredElement>, VectorResourceError> {
         let client = reqwest::blocking::Client::new();
 
         let part = blocking_multipart::Part::bytes(file_buffer)
@@ -39,14 +75,17 @@ impl UnstructuredAPI {
 
         let json: JsonValue = serde_json::from_str(&body)?;
 
-        Ok(json)
+        let elements = UnstructuredParser::parse_response_json(json)?;
+        Ok(elements)
     }
 
+    /// Makes an async request to process a file in a buffer into a list of
+    /// UnstructuredElements
     async fn process_file_request_async(
         &self,
         file_buffer: Vec<u8>,
         file_name: &str,
-    ) -> Result<JsonValue, VectorResourceError> {
+    ) -> Result<Vec<UnstructuredElement>, VectorResourceError> {
         let client = reqwest::Client::new();
 
         let part = multipart::Part::bytes(file_buffer)
@@ -66,14 +105,17 @@ impl UnstructuredAPI {
 
         let json: JsonValue = serde_json::from_str(&body)?;
 
-        Ok(json)
+        let elements = UnstructuredParser::parse_response_json(json)?;
+        Ok(elements)
     }
 }
 
+/// Struct which contains several methods related to parsing output from Unstructured
 #[derive(Debug)]
-pub struct UnstructuredResponseParser;
+pub struct UnstructuredParser;
 
-impl UnstructuredResponseParser {
+impl UnstructuredParser {
+    /// Parses the JSON Array response from Unstructured into a list of `UnstructuredElement`s
     pub fn parse_response_json(json: JsonValue) -> Result<Vec<UnstructuredElement>, VectorResourceError> {
         if let JsonValue::Array(array) = json {
             let mut elements = Vec::new();
@@ -89,48 +131,53 @@ impl UnstructuredResponseParser {
             ))
         }
     }
+
+    /// Extracts the most important keywords from a given text,
+    /// using the RAKE algorithm.
+    pub fn extract_keywords(elements: &Vec<UnstructuredElement>, num: u64) -> Vec<String> {
+        // Extract all the text out of all the elements and combine them together into a single string
+        let text = elements
+            .iter()
+            .map(|element| element.text.as_str())
+            .collect::<Vec<&str>>()
+            .join(" ");
+
+        // Create a new KeyPhraseExtractor with a maximum of num keywords
+        let extractor = KeyPhraseExtractor::new(&text, num as usize);
+
+        // Get the keywords
+        let keywords = extractor.get_keywords();
+
+        // Printing logic
+        // keywords
+        //     .iter()
+        //     .for_each(|(score, keyword)| println!("{}: {}", keyword, score));
+
+        // Return only the keywords, discarding the scores
+        keywords.into_iter().map(|(_score, keyword)| keyword).collect()
+    }
+
+    /// Generates a Blake3 hash of the data in the buffer
+    pub fn generate_data_hash(buffer: &[u8]) -> String {
+        let mut hasher = Hasher::new();
+        hasher.update(buffer);
+        let result = hasher.finalize();
+        result.to_hex().to_string()
+    }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq)]
+pub enum ElementType {
+    Title,
+    NarrativeText,
+    UncategorizedText,
+    ListItem,
 }
 
 #[derive(Debug, Deserialize)]
-#[serde(untagged)]
-pub enum UnstructuredElement {
-    Title(Title),
-    NarrativeText(NarrativeText),
-    UncategorizedText(UncategorizedText),
-    ListItem(ListItem),
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Title {
+pub struct UnstructuredElement {
     #[serde(rename = "type")]
-    pub element_type: String,
-    pub element_id: String,
-    pub metadata: Metadata,
-    pub text: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct NarrativeText {
-    #[serde(rename = "type")]
-    pub element_type: String,
-    pub element_id: String,
-    pub metadata: Metadata,
-    pub text: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct UncategorizedText {
-    #[serde(rename = "type")]
-    pub element_type: String,
-    pub element_id: String,
-    pub metadata: Metadata,
-    pub text: String,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct ListItem {
-    #[serde(rename = "type")]
-    pub element_type: String,
+    pub element_type: ElementType,
     pub element_id: String,
     pub metadata: Metadata,
     pub text: String,
--- shinkai-libs/shinkai-vector-resources/tests/unstructured_tests.rs
+++ shinkai-libs/shinkai-vector-resources/tests/unstructured_tests.rs
@@ -4,9 +4,9 @@ use shinkai_vector_resources::unstructured::*;
 use std::fs;
 
 lazy_static! {
-    pub static ref UNSTRUCTURED_API_URL: &'static str =
-        "https://internal.shinkai.com/x-unstructured-api/general/v0/general";
-    // pub static ref UNSTRUCTURED_API_URL: &'static str = "http://34.41.225.139/x-unstructured-api/general/v0/general";
+    // pub static ref UNSTRUCTURED_API_URL: &'static str =
+    //     "https://internal.shinkai.com/x-unstructured-api/general/v0/general";
+    pub static ref UNSTRUCTURED_API_URL: &'static str = "http://34.41.225.139/x-unstructured-api/general/v0/general";
 }
 
 #[test]
@@ -27,17 +27,16 @@ fn test_unstructured_parse_response_json() {
         "#;
 
     let json_value: JsonValue = serde_json::from_str(json_str).unwrap();
-    let result = UnstructuredResponseParser::parse_response_json(json_value).unwrap();
+    let result = UnstructuredParser::parse_response_json(json_value).unwrap();
 
     assert_eq!(result.len(), 1);
-    if let UnstructuredElement::Title(title) = &result[0] {
-        assert_eq!(title.element_type, "Title");
-        assert_eq!(title.element_id, "c674a556a00e31fb747e81263a3584be");
-        assert_eq!(title.metadata.filename, "Zeko_Mina_Rollup.pdf");
-        assert_eq!(title.metadata.filetype, "application/pdf");
-        assert_eq!(title.metadata.page_number, Some(1));
+    if ElementType::Title == result[0].element_type {
+        assert_eq!(result[0].element_id, "c674a556a00e31fb747e81263a3584be");
+        assert_eq!(result[0].metadata.filename, "Zeko_Mina_Rollup.pdf");
+        assert_eq!(result[0].metadata.filetype, "application/pdf");
+        assert_eq!(result[0].metadata.page_number, Some(1));
         assert_eq!(
-            title.text,
+            result[0].text,
             "Zeko: Fractal scaling of ZK applications using a Shared Sequencer L2 Stack"
         );
     } else {
@@ -54,12 +53,15 @@ fn test_unstructured_parse_pdf() {
     let file_buffer = fs::read(file_path).unwrap();
 
     // Create an UnstructuredAPI and process the file
-    let api = UnstructuredAPI::new(UNSTRUCTURED_API_URL.to_string());
-    let response = api.process_file_request_blocking(file_buffer, file_name).unwrap();
-
-    // Parse the response into unstructured elements
-    let elements = UnstructuredResponseParser::parse_response_json(response).unwrap();
+    let api = UnstructuredAPI::new(UNSTRUCTURED_API_URL.to_string(), None);
+    let elements = api.process_file_request_blocking(file_buffer, file_name).unwrap();
 
     // Check the parsed elements
-    // ...
+
+    // Print all elements for debugging
+    for element in &elements {
+        println!("{:?}", element);
+    }
+
+    assert_eq!(1, 2);
 }
