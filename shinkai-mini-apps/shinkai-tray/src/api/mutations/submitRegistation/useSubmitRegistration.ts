35435dede9408363e8e40ed84ea928beea9f7d7f
Paul Ccari <46382556+paulclindo@users.noreply.github.com>
2023-10-06 20:08:47 -0500

    Fixes on submit registration (#98)

--- shinkai-mini-apps/shinkai-tray/src/api/mutations/submitRegistation/useSubmitRegistration.ts
+++ shinkai-mini-apps/shinkai-tray/src/api/mutations/submitRegistation/useSubmitRegistration.ts
@@ -1,15 +1,10 @@
-import type { APIUseRegistrationCodeSuccessResponse } from "@shinkai_network/shinkai-message-ts/models";
 import type { UseMutationOptions } from "@tanstack/react-query";
 
 import { useMutation } from "@tanstack/react-query";
 
 import { SetupDataArgs, submitRegistration } from ".";
 
-type Data = {
-  success: boolean;
-  data?: APIUseRegistrationCodeSuccessResponse | undefined;
-};
-type Options = UseMutationOptions<Data, Error, SetupDataArgs>;
+type Options = UseMutationOptions<boolean, Error, SetupDataArgs>;
 
 export const useSubmitRegistration = (options?: Options) => {
   return useMutation({
