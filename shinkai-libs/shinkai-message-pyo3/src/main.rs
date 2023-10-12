a8c8e00133d9409e5f3fbcd2f1e64631d67f4d96
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-10-11 23:03:26 -0500

    fix script and test

--- shinkai-libs/shinkai-message-pyo3/src/main.rs
+++ shinkai-libs/shinkai-message-pyo3/src/main.rs
@@ -1,5 +1,4 @@
 pub mod shinkai_pyo3_wrapper;
-
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
--- shinkai-libs/shinkai-message-pyo3/tests/test_shinkai_message_pyo3.py
+++ shinkai-libs/shinkai-message-pyo3/tests/test_shinkai_message_pyo3.py
@@ -35,5 +35,42 @@ class TestShinkaiMessagePyO3(unittest.TestCase):
         self.assertEqual(result_json["encryption"], "None")
         self.assertEqual(result_json["version"], "V1_0")
 
+    def test_initial_registration_with_no_code_for_device(self):
+        my_encryption_sk_string = '7008829b80ae4350cf049e48d8bce4714e216b674fff0bf34f97f7b98d928d3f'
+        my_encryption_pk_string = '5b0d4a7f7135ebe6712a65256b9bcb2cf79ee7425407da3cbb51f07dd9d68235'
+
+        my_identity_sk_string = 'b6baf0fa268f993c57223d5db96e5e1de776fcb0195ee6137f33de9d8d9dd749'
+        my_identity_pk_string = '4e91b8ca811cdb07c636190e3f1bc39edcf8ac47cfd4d1c3267fec3be570e740'
+        node = "@@node1.shinkai"
+
+        # _registration_with_no_code_for_device
+        result = shinkai_message_pyo3.PyShinkaiMessageBuilder.initial_registration_with_no_code_for_device( 
+            my_encryption_sk_string,  # device
+            my_identity_sk_string,  # device
+            my_encryption_sk_string,  # profile
+            my_identity_sk_string,  # profile
+            "main_device",
+            node,
+            "",
+            node,
+        )
+
+        # print("Result of initial registration no code:", result)
+
+        # Parse the result as a JSON object
+        result_json = json.loads(result)
+
+        # Add assertions to check the fields of the result
+        self.assertEqual(result_json["body"]["unencrypted"]["message_data"]["unencrypted"]["message_raw_content"], '{"code":"","registration_name":"main_device","device_identity_pk":"4e91b8ca811cdb07c636190e3f1bc39edcf8ac47cfd4d1c3267fec3be570e740","device_encryption_pk":"5b0d4a7f7135ebe6712a65256b9bcb2cf79ee7425407da3cbb51f07dd9d68235","profile_identity_pk":"4e91b8ca811cdb07c636190e3f1bc39edcf8ac47cfd4d1c3267fec3be570e740","profile_encryption_pk":"5b0d4a7f7135ebe6712a65256b9bcb2cf79ee7425407da3cbb51f07dd9d68235","identity_type":"device","permission_type":"admin"}')
+        self.assertEqual(result_json["body"]["unencrypted"]["message_data"]["unencrypted"]["message_content_schema"], "UseRegistrationCode")
+        self.assertEqual(result_json["body"]["unencrypted"]["internal_metadata"]["sender_subidentity"], "")
+        self.assertEqual(result_json["body"]["unencrypted"]["internal_metadata"]["recipient_subidentity"], "")
+        self.assertEqual(result_json["body"]["unencrypted"]["internal_metadata"]["inbox"], "inbox::@@node1.shinkai::@@node1.shinkai::false")
+        self.assertEqual(result_json["body"]["unencrypted"]["internal_metadata"]["encryption"], "None")
+        self.assertEqual(result_json["external_metadata"]["sender"], "@@node1.shinkai")
+        self.assertEqual(result_json["external_metadata"]["recipient"], "@@node1.shinkai")
+        self.assertEqual(result_json["encryption"], "None")
+        self.assertEqual(result_json["version"], "V1_0")
+
 if __name__ == '__main__':
     unittest.main()
