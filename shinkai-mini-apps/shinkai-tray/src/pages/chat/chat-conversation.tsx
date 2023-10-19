a7d9cb85c2e69aafce4da21c65ac1b35275f1642
paulclindo <paulclindo@gmail.com>
2023-10-19 17:19:18 -0500

    fix file type

--- shinkai-mini-apps/shinkai-tray/src/pages/chat/chat-conversation.tsx
+++ shinkai-mini-apps/shinkai-tray/src/pages/chat/chat-conversation.tsx
@@ -92,11 +92,6 @@ const ChatConversation = () => {
   const { getRootProps: getRootFileProps, getInputProps: getInputFileProps } =
     useDropzone({
       multiple: false,
-      accept: {
-        "image/jpg": [],
-        "image/jpeg": [],
-        "image/png": [],
-      },
       onDrop: (acceptedFiles) => {
         const file = acceptedFiles[0];
         const reader = new FileReader();
@@ -328,8 +323,9 @@ const ChatConversation = () => {
                                     : "rounded-tr-none border-none bg-[rgba(217,217,217,0.04)]"
                                 )}
                                 source={`${
-                                  getMessageFilesInbox(message) &&
-                                  `<svg xmlns="http://www.w3.org/2000/svg" className="mb-2" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><line x1="16" x2="8" y1="13" y2="13"/><line x1="16" x2="8" y1="17" y2="17"/><line x1="10" x2="8" y1="9" y2="9"/></svg>`
+                                  getMessageFilesInbox(message)
+                                    ? `<svg xmlns="http://www.w3.org/2000/svg" className="mb-2" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><line x1="16" x2="8" y1="13" y2="13"/><line x1="16" x2="8" y1="17" y2="17"/><line x1="10" x2="8" y1="9" y2="9"/></svg>`
+                                    : ""
                                 } ${getMessageContent(message)}`}
                                 wrapperElement={{
                                   "data-color-mode": "dark",
