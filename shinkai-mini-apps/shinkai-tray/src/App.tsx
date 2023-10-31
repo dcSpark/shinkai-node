46e8493b70476f89ac5891613578fad0b312dfc5
paulclindo <paulclindo@gmail.com>
2023-10-31 00:23:40 -0500

    fix: router

--- shinkai-mini-apps/shinkai-tray/src/App.tsx
+++ shinkai-mini-apps/shinkai-tray/src/App.tsx
@@ -1,4 +1,4 @@
-import { MemoryRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
