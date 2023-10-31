1586c632a94a2e19a6f8f108b423749793f95250
paulclindo <paulclindo@gmail.com>
2023-10-31 14:29:25 -0500

    wip smart inboxes

--- shinkai-mini-apps/shinkai-tray/src/api/queries/getInboxes/index.ts
+++ shinkai-mini-apps/shinkai-tray/src/api/queries/getInboxes/index.ts
@@ -1,6 +1,48 @@
-import { getAllInboxesForProfile } from "@shinkai_network/shinkai-message-ts/api";
+import type { CredentialsPayload } from "@shinkai_network/shinkai-message-ts/models";
+
+import {
+  ApiConfig,
+  getAllInboxesForProfile,
+  handleHttpError,
+} from "@shinkai_network/shinkai-message-ts/api";
+import { ShinkaiMessageBuilderWrapper } from "@shinkai_network/shinkai-message-ts/wasm";
 
 import type { GetInboxesInput } from "./types";
+
+export const getAllSmartInboxes = async (
+  sender: string,
+  sender_subidentity: string,
+  receiver: string,
+  target_shinkai_name_profile: string,
+  setupDetailsState: CredentialsPayload
+): Promise<string[]> => {
+  try {
+    const messageString = ShinkaiMessageBuilderWrapper.get_all_inboxes_for_profile(
+      setupDetailsState.my_device_encryption_sk,
+      setupDetailsState.my_device_identity_sk,
+      setupDetailsState.node_encryption_pk,
+      sender + "/" + sender_subidentity,
+      "",
+      receiver,
+      target_shinkai_name_profile
+    );
+
+    const message = JSON.parse(messageString);
+
+    const apiEndpoint = ApiConfig.getInstance().getEndpoint();
+    const response = await fetch(`${apiEndpoint}/v1/get_all_smart_inboxes_for_profile`, {
+      method: "POST",
+      body: JSON.stringify(message),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    await handleHttpError(response);
+    return data.data;
+  } catch (error) {
+    console.error("Error getting all inboxes for profile:", error);
+    throw error;
+  }
+};
 
 export const getInboxes = async ({
   receiver,
@@ -13,7 +55,7 @@ export const getInboxes = async ({
   profile_encryption_sk,
   profile_identity_sk,
 }: GetInboxesInput) => {
-  const inboxes = await getAllInboxesForProfile(
+  const inboxes = await getAllSmartInboxes(
     sender,
     senderSubidentity,
     receiver,
@@ -26,6 +68,7 @@ export const getInboxes = async ({
       profile_identity_sk,
     }
   );
-  // TODO:
-  return inboxes.map((inbox) => encodeURIComponent(inbox));
+  console.log("SMART", inboxes);
+  // TODO: fix types
+  return inboxes.map((inbox) => encodeURIComponent(inbox.inbox_id));
 };
