704c9c4c827c1ae27d8b0f28d693701e9d050807
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-11-12 20:14:43 -0600

    cp

--- tests/planner_tests.rs
+++ tests/planner_tests.rs
@@ -2,159 +2,108 @@
 mod tests {
     use super::*;
     use futures::Future;
-    use shinkai_node::planner::runner::{Plan, Task};
+    use pddl_rs::{
+        compiler::compile_problem,
+        parser::{parse_domain, parse_problem},
+        Error, Sources,
+    };
+
+    use ariadne::{Color, Label, Report, ReportKind, Source};
+    use std::{io::Cursor, path::PathBuf};
     use std::{pin::Pin, sync::Arc};
     use tokio::sync::Mutex;
 
-    fn mock_execute_task(_task: &Task) -> Pin<Box<dyn Future<Output = Result<(), &'static str>> + Send>> {
-        // Mock implementation here
-        eprintln!("Mock task execution OK");
-        Box::pin(async { Ok(()) })
+    static DOMAIN_PDDL: &str = r#"(define (domain AI-news-summary)
+        (:requirements :strips :typing)
+        (:types url html link content summary)
+
+        (:predicates
+            (htmlFetched ?url - url ?html - html)
+            (linksExtracted ?html - html ?link - link)
+            (contentFetched ?link - link ?content - content)
+            (summaryGenerated ?content - content ?summary - summary)
+        )
+
+        (:action fetchHtml
+            :parameters (?url - url)
+            :effect (htmlFetched ?url)
+        )
+
+        (:action extractLinks
+            :parameters (?html - html)
+            :precondition (htmlFetched ?url ?html)
+            :effect (linksExtracted ?html)
+        )
+
+        (:action fetchContent
+            :parameters (?link - link)
+            :precondition (linksExtracted ?html ?link)
+            :effect (contentFetched ?link)
+        )
+
+        (:action generateSummary
+            :parameters (?content - content)
+            :precondition (contentFetched ?link ?content)
+            :effect (summaryGenerated ?content)
+        )
+)"#;
+
+    static PROBLEM_PDDL: &str = r#"(define (problem example)
+    (:domain AI-news-summary)
+    (:objects 
+        myurl - url
+        myhtml - html
+        mylink - link
+        mycontent - content
+    )
+    (:init 
+        ; Assuming 'myurl' is the URL we start with
+        ; Other objects start without any initial state
+    )
+    (:goal 
+        (and
+            (htmlFetched myurl myhtml)
+            (linksExtracted myhtml mylink)
+            (contentFetched mylink mycontent)
+            (summaryGenerated mycontent)
+        )
+    )
+)"#;
+
+
+    fn print_pddl_error(input: &str, error: &Error) {
+        eprintln!("Error parsing PDDL: {:?}", error);
     }
 
-    #[tokio::test]
-    async fn test_process_plan() {
-        let json = r#"
-        {
-            "tasks": [
-                {
-                    "task_id": "task_id_1",
-                    "task_inputs_schema": ["string", "string"],
-                    "task_inputs": ["input1", "input2"],
-                    "description": "some description",
-                    "task_query": "Find all the news related to {}",
-                    "query_inputs": [0],
-                    "task_query_output": ["some_result with: one %% two %% three"],
-                    "post_processing:": [
-                        {
-                            "mini_tool": "splitter",
-                            "param": "%%"
-                        },
-                        {
-                            "mini_tool": "regex",
-                            "param": "some_regex"
-                        }
-                    ],
-                    "post_processing_results": [
-                        "one", "two", "three"
-                    ],
-                    "task_outputs": [
-                        {
-                            "task_inputs": "string",
-                            "index": 1
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 0
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 1
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 2
-                        }
-                    ],
-                    "state": "completed",
-                    "success": {
-                        "next": "task_id_2",
-                        "message": {
-                            "query": "Step completed successfully with result {}",
-                            "refs": ["output:0"]
-                        }
-                    },
-                    "failure": {
-                        "next": "",
-                        "message": {
-                            "query": "This task failed for input {}. Executing error handling.",
-                            "refs": ["input:0"]
-                        }
-                    }
-                },
-                {
-                    "task_id": "task_id_2",
-                    "task_inputs": ["string", "string"],
-                    "description": "some description",
-                    "task_query": "Find all the news related to {}",
-                    "query_inputs": [0],
-                    "task_query_output": ["some_result with: one %% two %% three"],
-                    "post_processing:": [
-                        {
-                            "mini_tool": "splitter",
-                            "param": "%%"
-                        },
-                        {
-                            "mini_tool": "regex",
-                            "param": "some_regex"
-                        }
-                    ],
-                    "post_processing_results": [
-                        "one", "two", "three"
-                    ],
-                    "task_outputs": [
-                        {
-                            "task_inputs": "string",
-                            "index": 1
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 0
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 1
-                        },
-                        {
-                            "post_processing_results": "string",
-                            "index": 2
-                        }
-                    ],
-                    "state": "completed",
-                    "success": {
-                        "next": "task_id_2",
-                        "message": {
-                            "query": "Step completed successfully with result {}",
-                            "refs": ["output:0"]
-                        }
-                    },
-                    "failure": {
-                        "next": "",
-                        "message": {
-                            "query": "This task failed for input {}. Executing error handling.",
-                            "refs": ["input:0"]
-                        }
-                    }
-                },
-            ],
-            "states": {
-                "initial": {
-                    "task_id": "task_id_1",
-                    "inputs": ["input1", "input2"]
-                },
-                "processed": [],
-                "completed": false
-            },
-            "iteration_control": {
-                "max_iterations": 10,
-                "current_iteration": 0
-            }
+    #[test]
+    fn test_ai_news_summary_pddl() {
+        // Create Sources instance from strings
+        let sources = Sources {
+            domain_path: "embedded_domain".into(),
+            problem_path: "embedded_problem".into(),
+            domain_ad: Source::from(DOMAIN_PDDL.to_string()),
+            problem_ad: Source::from(PROBLEM_PDDL.to_string()),
+            domain_src: DOMAIN_PDDL.to_string(),
+            problem_src: PROBLEM_PDDL.to_string(),
+        };
+
+        // Parse domain
+        let domain_result = parse_domain(&sources.domain_src);
+        if let Err(ref e) = domain_result {
+            print_pddl_error(&sources.domain_src, e);
         }
-        "#;
 
-        // first validate plan
-        let errors = Plan::validate_plan(json).unwrap();
-        eprintln!("Errors: {:?}", errors);
-        assert_eq!(errors.len(), 0);
-
-        let mut plan: Plan = serde_json::from_str(json).unwrap();
-        plan.execute_task = mock_execute_task;
-
-        let plan = Arc::new(Mutex::new(plan));
-        let _ = Plan::process_plan(Arc::clone(&plan)).await;
-
-        let plan_guard = plan.lock().await;
-        assert_eq!(plan_guard.states.processed.len(), 2);
-        assert!(plan_guard.states.completed);
+        match parse_domain(&sources.domain_src) {
+            Ok(domain) => match parse_problem(&sources.problem_src, domain.requirements) {
+                Ok(problem) => match compile_problem(&domain, &problem) {
+                    Ok(_c_problem) => {
+                        eprintln!("CompiledProblem: {:?}", _c_problem);
+                    }
+                    Err(e) => eprintln!("Error compiling problem inside : {:?}", e),
+                },
+                Err(e) => eprintln!("Error parsing problem inside: {:?}", e),
+            },
+            Err(e) => eprintln!("Error parsing domain inside: {:?}", e),
+        }
     }
 }
