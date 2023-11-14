20d54bf3d1152f7f2cf37e220ac60a409be41ad3
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-11-13 18:41:19 -0600

    log

--- shinkai-libs/shinkai-message-primitives/src/shinkai_utils/shinkai_logging.rs
+++ shinkai-libs/shinkai-message-primitives/src/shinkai_utils/shinkai_logging.rs
@@ -1,5 +1,5 @@
-use colored::*;
 use chrono::Local;
+use colored::*;
 
 #[derive(PartialEq, Debug)]
 pub enum ShinkaiLogOption {
@@ -47,7 +47,7 @@ fn active_log_options() -> Vec<ShinkaiLogOption> {
             ShinkaiLogOption::Tests,
         ];
     }
-    
+
     let mut active_options = Vec::new();
     if std::env::var("LOG_BLOCKCHAIN").is_ok() {
         active_options.push(ShinkaiLogOption::Blockchain);
@@ -79,35 +79,38 @@ fn active_log_options() -> Vec<ShinkaiLogOption> {
     if std::env::var("LOG_JOB_EXECUTION").is_ok() {
         active_options.push(ShinkaiLogOption::JobExecution);
     }
-    
+
     active_options
 }
 
 pub fn shinkai_log(option: ShinkaiLogOption, level: ShinkaiLogLevel, message: &str) {
     let active_options = active_log_options();
     if active_options.contains(&option) {
-        let hostname = "localhost";
-        let app_name = "shinkai";
-        let proc_id = std::process::id().to_string();
-        let msg_id = "-"; // No specific message ID
-
         let time = Local::now().format("%Y-%m-%dT%H:%M:%S%.fZ"); // RFC 3339 timestamp
         let option_str = format!("{:?}", option);
-        let header = format!("{} {} {} {} {}", time, hostname, app_name, proc_id, msg_id);
 
         let (level_str, color_fn): (&str, Box<dyn Fn(&str) -> ColoredString>) = match level {
-            ShinkaiLogLevel::Error => ("ERROR", Box::new(|s: &str| s.red())),
-            ShinkaiLogLevel::Info => ("INFO", Box::new(|s: &str| s.yellow())),
-            ShinkaiLogLevel::Debug => ("DEBUG", Box::new(|s: &str| s.normal())),
+            ShinkaiLogLevel::Error => ("(ERROR)", Box::new(|s: &str| s.red())),
+            ShinkaiLogLevel::Info => ("(INFO)", Box::new(|s: &str| s.yellow())),
+            ShinkaiLogLevel::Debug => ("(DEBUG)", Box::new(|s: &str| s.normal())),
         };
 
-        let message_with_header = format!("{} - {} - {} - {}", header, level_str, option_str, message);
+        let message_with_header = if std::env::var("LOG_SIMPLE").is_ok() {
+            format!("{} {} - {} - {}", time, level_str, option_str, message)
+        } else {
+            let hostname = "localhost";
+            let app_name = "shinkai";
+            let proc_id = std::process::id().to_string();
+            let msg_id = "-"; // No specific message ID
+            let header = format!("{} {} {} {} {}", time, hostname, app_name, proc_id, msg_id);
+            format!("{} - {} - {} - {}", header, level_str, option_str, message)
+        };
 
         match level.to_log_level() {
             log::Level::Error => eprintln!("{}", color_fn(&message_with_header)),
             log::Level::Info => println!("{}", color_fn(&message_with_header)),
             log::Level::Debug => println!("{}", color_fn(&message_with_header)),
-            _ => {},
+            _ => {}
         }
     }
 }
