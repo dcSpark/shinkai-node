150073578bb45df8f17b7dad7fe06cd2cce080c5
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-12-07 17:39:27 -0600

    fix test

--- src/crypto_identities/crypto_identity_manager.rs
+++ src/crypto_identities/crypto_identity_manager.rs
@@ -1,74 +1,155 @@
-// use std::collections::HashMap;
-// use std::convert::TryFrom;
-// use alloy_rs::prelude::*;
-// use std::fs;
+use ethers::abi::Abi;
+use ethers::prelude::*;
+use shinkai_message_primitives::shinkai_utils::shinkai_logging::ShinkaiLogLevel;
+use shinkai_message_primitives::shinkai_utils::shinkai_logging::ShinkaiLogOption;
+use shinkai_message_primitives::shinkai_utils::shinkai_logging::shinkai_log;
+use std::convert::TryFrom;
+use std::fmt;
+use std::fs;
+use std::sync::Arc;
 
-// #[derive(Debug)]
-// pub struct OnchainIdentity {
-//     pub encryption_key: String,
-//     pub signature_key: String,
-//     pub staked_tokens: u64,
-//     pub bound_nft: u64, // id of the nft
-//     pub routing: bool,
-//     pub address_or_proxy_nodes: Vec<String>,
-//     pub delegated_tokens: u64,
-// }
+#[derive(Debug)]
+pub enum ShinkaiRegistryError {
+    UrlParseError(rustube::url::ParseError),
+    ProviderError(ethers::providers::ProviderError),
+    ContractAbiError(ethers::contract::AbiError),
+    AbiError(ethers::abi::Error),
+    IoError(std::io::Error),
+    JsonError(serde_json::Error),
+    CustomError(String),
+}
 
-// struct ShinkaiRegistry {
-//     contract: Contract<Http>,
-//     // identity_records: HashMap<String, IdentityRecord>,
-//     // ... add other fields as needed
-// }
+impl fmt::Display for ShinkaiRegistryError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ShinkaiRegistryError::UrlParseError(err) => write!(f, "URL Parse Error: {}", err),
+            ShinkaiRegistryError::ProviderError(err) => write!(f, "Provider Error: {}", err),
+            // ShinkaiRegistryError::ContractError(err) => write!(f, "Contract Error: {}", err),
+            ShinkaiRegistryError::ContractAbiError(err) => write!(f, "Contract ABI Error: {}", err),
+            ShinkaiRegistryError::AbiError(err) => write!(f, "ABI Error: {}", err),
+            ShinkaiRegistryError::IoError(err) => write!(f, "IO Error: {}", err),
+            ShinkaiRegistryError::JsonError(err) => write!(f, "JSON Error: {}", err),
+            ShinkaiRegistryError::CustomError(err) => write!(f, "Custom Error: {}", err),
+        }
+    }
+}
 
-// impl ShinkaiRegistry {
-//     // Initialize a new ShinkaiRegistry
-//     pub async fn new(url: &str, contract_address: &str, abi_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-//         let provider = Provider::<Http>::try_from(url)?;
-//         let contract_address: Address = contract_address.parse()?;
+impl From<ethers::contract::AbiError> for ShinkaiRegistryError {
+    fn from(err: ethers::contract::AbiError) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::ContractAbiError(err)
+    }
+}
 
-//         // Read the ABI from the file
-//         let abi = fs::read(abi_path)?;
+impl From<serde_json::Error> for ShinkaiRegistryError {
+    fn from(err: serde_json::Error) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::JsonError(err)
+    }
+}
 
-//         let contract = Contract::from_json(
-//             provider,
-//             contract_address,
-//             &abi,
-//         )?;
+impl From<ethers::providers::ProviderError> for ShinkaiRegistryError {
+    fn from(err: ethers::providers::ProviderError) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::ProviderError(err)
+    }
+}
 
-//         Ok(Self { contract })
-//     }
+impl From<ethers::abi::Error> for ShinkaiRegistryError {
+    fn from(err: ethers::abi::Error) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::AbiError(err)
+    }
+}
 
-//     pub async fn get_identity_record(&self, identity: String) -> Result<IdentityRecord, Box<dyn std::error::Error>> {
-//         let result: (String, String, u64, u64, bool, Vec<String>, u64) = self.contract
-//             .query("getIdentityRecord", (identity,), None, Options::default(), None)
-//             .await?;
+impl From<std::io::Error> for ShinkaiRegistryError {
+    fn from(err: std::io::Error) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::IoError(err)
+    }
+}
 
-//         Ok(IdentityRecord {
-//             encryption_key: result.0,
-//             signature_key: result.1,
-//             staked_tokens: result.2,
-//             bound_nft: result.3,
-//             routing: result.4,
-//             address_or_proxy_nodes: result.5,
-//             delegated_tokens: result.6,
-//         })
-//     }
-// }
+impl From<rustube::url::ParseError> for ShinkaiRegistryError {
+    fn from(err: rustube::url::ParseError) -> ShinkaiRegistryError {
+        ShinkaiRegistryError::UrlParseError(err)
+    }
+}
 
+#[derive(Debug, PartialEq, Clone)]
+pub struct OnchainIdentity {
+    pub bound_nft: U256, // id of the nft
+    pub staked_tokens: U256,
+    pub encryption_key: String,
+    pub signature_key: String,
+    pub routing: bool,
+    pub address_or_proxy_nodes: Vec<String>,
+    pub delegated_tokens: U256,
+}
 
-// /*
-//     - Create Identity Manager (Reader)
-//     - it should have an indexer for caching
-//     - it should be able to check if the indexer is up to date easily (maybe with a timestamp)
-//     - if indexer not up to date, it should be able to update it and (next line)
-//     - it should be able to do individual request to an external or local node api
-//     - it should be able to read current information associated with the user (delegation, info of stake pools, etc)
+#[derive(Debug, Clone)]
+pub struct ShinkaiRegistry {
+    pub contract: ContractInstance<Arc<Provider<Http>>, Provider<Http>>,
+    // identity_records: HashMap<String, IdentityRecord>,
+    // ... add other fields as needed
+}
 
-//     // Should we have a different identity manager for writing? It feels like we should.
-//     // add local mnemonics
-//     // rpc (local or external) to create new identities
+impl ShinkaiRegistry {
+    // Initialize a new ShinkaiRegistry
+    pub async fn new(url: &str, contract_address: &str, abi_path: &str) -> Result<Self, ShinkaiRegistryError> {
+        let provider = Provider::<Http>::try_from(url).map_err(ShinkaiRegistryError::UrlParseError)?;
+        let contract_address: Address = contract_address.parse().map_err(|e| {
+            shinkai_log(ShinkaiLogOption::CryptoIdentity, ShinkaiLogLevel::Error, format!("Error parsing contract address: {}", e).as_str());
+            ShinkaiRegistryError::AbiError(ethers::abi::Error::InvalidData)
+        })?;
 
-//     // some values:
-//     TESTNET_RPC=https://eth-sepolia.g.alchemy.com/v2/demo
-//     MAINNET_RPC=https://eth.llamarpc.com
-// */
+        let abi = fs::read_to_string(abi_path).map_err(ShinkaiRegistryError::IoError)?;
+        let abi: Abi = serde_json::from_str(&abi).map_err(ShinkaiRegistryError::JsonError)?;
+
+        let contract = Contract::new(contract_address, abi, Arc::new(provider));
+        Ok(Self { contract })
+    }
+
+    pub async fn get_identity_record(&self, identity: String) -> Result<OnchainIdentity, ShinkaiRegistryError> {
+        let function_call = match self
+            .contract
+            .method::<_, (U256, U256, String, String, bool, Vec<String>, U256)>("getIdentityRecord", (identity,))
+        {
+            Ok(call) => call,
+            Err(err) => {
+                shinkai_log(ShinkaiLogOption::CryptoIdentity, ShinkaiLogLevel::Error, format!("Error creating function call: {}", err).as_str());
+                return Err(ShinkaiRegistryError::ContractAbiError(err));
+            }
+        };
+
+        let result: (U256, U256, String, String, bool, Vec<String>, U256) = match function_call.call().await {
+            Ok(res) => res,
+            Err(e) => {
+                shinkai_log(ShinkaiLogOption::CryptoIdentity, ShinkaiLogLevel::Error, format!("Error calling contract: {}", e).as_str());
+                return Err(ShinkaiRegistryError::CustomError("Contract Error".to_string()));
+            }
+        };
+        shinkai_log(ShinkaiLogOption::CryptoIdentity, ShinkaiLogLevel::Debug, format!("get_identity_record result: {:?}", result).as_str());
+
+        Ok(OnchainIdentity {
+            bound_nft: result.0,
+            staked_tokens: result.1,
+            encryption_key: result.2,
+            signature_key: result.3,
+            routing: result.4,
+            address_or_proxy_nodes: result.5,
+            delegated_tokens: result.6,
+        })
+    }
+}
+
+/*
+    - Create Identity Manager (Reader)
+    - it should have an indexer for caching
+    - it should be able to check if the indexer is up to date easily (maybe with a timestamp)
+    - if indexer not up to date, it should be able to update it and (next line)
+    - it should be able to do individual request to an external or local node api
+    - it should be able to read current information associated with the user (delegation, info of stake pools, etc)
+
+    // Should we have a different identity manager for writing? It feels like we should.
+    // add local mnemonics
+    // rpc (local or external) to create new identities
+
+    // some values:
+    TESTNET_RPC=https://eth-sepolia.g.alchemy.com/v2/demo
+    MAINNET_RPC=https://eth.llamarpc.com
+*/
--- tests/get_onchain_identity_tests.rs
+++ tests/get_onchain_identity_tests.rs
@@ -1,23 +1,32 @@
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use tokio::runtime::Runtime;
+#[cfg(test)]
+mod tests {
+    use ethers::types::U256;
+    use shinkai_node::crypto_identities::crypto_identity_manager::{ShinkaiRegistry, OnchainIdentity};
+    use tokio::runtime::Runtime;
 
-//     #[test]
-//     fn test_get_identity_record() {
-//         let mut rt = Runtime::new().unwrap();
+    #[test]
+    fn test_get_identity_record() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let registry = ShinkaiRegistry::new(
+                "https://rpc.sepolia.org",
+                "0xb2945D0CDa4C119DE184380955aA4FbfAFb6B8cC",
+                "./src/crypto_identities/abi/ShinkaiRegistry.sol/ShinkaiRegistry.json",
+            ).await.unwrap();
 
-//         rt.block_on(async {
-//             let registry = ShinkaiRegistry::new(
-//                 "https://eth-sepolia.g.alchemy.com/v2/demo",
-//                 "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-//                 "/src/crypto_identities/abi/ShinkaiRegistry.sol/ShinkaiRegistry.json",
-//             ).await.unwrap();
+            let record = registry.get_identity_record("nico.shinkai".to_string()).await.unwrap();
 
-//             let record = registry.get_identity_record("identity_to_fetch".to_string()).await.unwrap();
-
-//             println!("{:?}", record);
-//         });
-//     }
-// }
+            let expected_record = OnchainIdentity {
+                bound_nft: U256::from_dec_str("11").unwrap(),
+                staked_tokens: U256::from_dec_str("62000000000000000000").unwrap(),
+                encryption_key: "858bef3bb7839329e28e569288f441f8fa86af00d9f41a9845ef50dd3b6cd15f".to_string(),
+                signature_key: "7aa221ec6761fdfdb478616babad8fad5330587392ad7e7dc9002af269909882".to_string(),
+                routing: false,
+                address_or_proxy_nodes: vec![],
+                delegated_tokens: U256::from_dec_str("0").unwrap(),
+            };
+            assert_eq!(record, expected_record);
+        });
+    }
+}
