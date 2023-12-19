c10dc659d43b588ead6e1e81289f7b838eb0e89a
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2023-12-19 19:40:28 +0700

    new test

--- tests/job_tree_usage_tests.rs
+++ tests/job_tree_usage_tests.rs
@@ -29,7 +29,7 @@ use mockito::Server;
 
 #[test]
 #[ignore]
-fn job_image_analysis() {
+fn job_tree_usage_tests() {
     run_test_one_node_network(|env| {
         Box::pin(async move {
             let node1_commands_sender = env.node1_commands_sender.clone();
@@ -84,17 +84,17 @@ fn job_image_analysis() {
                     .with_header("content-type", "application/json")
                     .with_body(
                         r#"{
-                            "model":"llava",
-                            "created_at":"2023-12-19T11:18:05.31733973Z",
-                            "response":"{\"answer\": \"A bright blue, clear sky\"}",
+                            "model":"mixtral:8x7b-instruct-v0.1-q4_1",
+                            "created_at":"2023-12-19T11:36:44.687874415Z",
+                            "response":"{\"answer\": \"Why couldn't the bicycle stand up by itself? Because it was two-tired.\"}",
                             "done":true,
-                            "context":[29871,13,11889,29901,887,526,263,1407,8444,20255,393,29915,29879,1407,1781,472,1614,1259,263,3414,29889,448,450,1857,1667,3414,472,1361,338,29901,421,2783,29581,278,1967,29952,2538,2818,773,278,1494,382,29933,22498,322,13312,3078,1683,29901,525,10998,525,12011,29915,525,11283,1347,525,10162,29871,7521,3126,13,22933,5425,1254,13566,29901,8853,12011,1115,376,29909,11785,7254,29892,2821,14744,9092],
-                            "total_duration":3482767354,
-                            "load_duration":2553548600,
-                            "prompt_eval_count":1,
-                            "prompt_eval_duration":798772000,
-                            "eval_count":11,
-                            "eval_duration":127775000
+                            "context":[28705,733,16289,28793,28705,995,460,396,10023,13892,693,865,659,2735,298,272,3857,3036,304,574,1216,4788,298,4372,707,2996,272,2188,5312,28723,2378,459,1460,354,3629,2758,442,1871,297,574,4372,298,272,2188,28725,562,3768,1912,272,2188,390,1188,1871,390,2572,28723,415,2188,659,2261,28747,28705,387,1912,528,264,13015,28723,13,1047,368,506,2066,1871,298,5090,4372,272,2188,28742,28713,2996,28747,1992,19571,1413,272,2296,413,6848,28765,304,7771,2511,1112,28747,464,18437,464,24115,28742,464,14243,1423,464,11339,28705,1047,368,927,298,18896,680,1871,298,9222,4372,272,2188,28725,868,368,622,927,298,1073,9547,304,1605,27674,4916,28748,14104,272,6594,14060,395,680,1871,304,1073,302,264,3472,5709,298,1300,633,3036,28723,11147,354,28049,680,4842,567,10537,821,1552,28707,479,528,264,28832,28747,1992,19571,1413,272,2296,413,6848,28765,304,7771,2511,1112,28747,464,18437,464,2360,28742,464,14243,1423,28725,464,3499,1869,464,1427,28742,443,28742,28705,8789,3371,733,28748,16289,4490,28739,24115,1264,345,7638,3481,28742,28707,272,24521,1812,1876,582,486,3837,28804,5518,378,403,989,28733,28707,1360,611,28752],
+                            "total_duration":29617027653,
+                            "load_duration":7157879293,
+                            "prompt_eval_count":203,
+                            "prompt_eval_duration":19022360000,
+                            "eval_count":25,
+                            "eval_duration":3435284000
                         }"#,
                     )
                     .create();
@@ -104,7 +104,7 @@ fn job_image_analysis() {
                 };
 
                 let ollama = Ollama {
-                    model_type: "llava".to_string(),
+                    model_type: "mixtral:8x7b-instruct-v0.1-q4_1".to_string(),
                 };
 
                 let api_key = env::var("INITIAL_AGENT_API_KEY").expect("API_KEY must be set");
@@ -115,10 +115,10 @@ fn job_image_analysis() {
                     perform_locally: false,
                     // external_url: Some("http://localhost:11435".to_string()),
                     // external_url: Some("https://api.openai.com".to_string()),
-                    // api_key: Some("".to_string()),
+                    api_key: Some("".to_string()),
                     // api_key: Some(api_key),
                     external_url: Some(server.url()),
-                    api_key: Some("mockapikey".to_string()),
+                    // api_key: Some("mockapikey".to_string()),
                     // external_url: Some("https://api.together.xyz".to_string()),
                     // model: AgentLLMInterface::OpenAI(open_ai),
                     // model: AgentLLMInterface::GenericAPI(generic_api),
@@ -155,61 +155,7 @@ fn job_image_analysis() {
                 )
                 .await;
             }
-            {
-                eprintln!("\n\n### Sending message (APICreateFilesInboxWithSymmetricKey) from profile subidentity to node 1\n\n");
-
-                let message_content = aes_encryption_key_to_string(symmetrical_sk.clone());
-                let msg = ShinkaiMessageBuilder::create_files_inbox_with_sym_key(
-                    node1_profile_encryption_sk.clone(),
-                    clone_signature_secret_key(&node1_profile_identity_sk),
-                    node1_encryption_pk,
-                    "job::test::false".to_string(),
-                    message_content.clone(),
-                    node1_profile_name.to_string(),
-                    node1_identity_name.to_string(),
-                    node1_identity_name.to_string(),
-                )
-                .unwrap();
-
-                let (res_sender, res_receiver) = async_channel::bounded(1);
-                node1_commands_sender
-                    .send(NodeCommand::APICreateFilesInboxWithSymmetricKey { msg, res: res_sender })
-                    .await
-                    .unwrap();
-                let response = res_receiver.recv().await.unwrap().expect("Failed to receive messages");
-            }
-            {
-                eprintln!("\n\n### Sending message (APIAddFileToInboxWithSymmetricKey) from profile subidentity to node 1\n\n");
-                let file_path = "files/blue_64x64.png";
-                let file_data = std::fs::read(file_path).expect("Failed to read file");
-
-                // Encrypt the file using Aes256Gcm
-                let cipher = Aes256Gcm::new(GenericArray::from_slice(&symmetrical_sk));
-                let nonce = GenericArray::from_slice(&[0u8; 12]);
-                let nonce_slice = nonce.as_slice();
-                let nonce_str = aes_nonce_to_hex_string(nonce_slice);
-                let ciphertext = cipher.encrypt(nonce, file_data.as_ref()).expect("encryption failure!");
-
-                // Prepare the response channel
-                let (res_sender, res_receiver) = async_channel::bounded(1);
-
-                // Send the command
-                node1_commands_sender
-                    .send(NodeCommand::APIAddFileToInboxWithSymmetricKey {
-                        filename: "samurai_underwater.png".to_string(),
-                        file: ciphertext,
-                        public_key: hash_of_aes_encryption_key_hex(symmetrical_sk),
-                        encrypted_nonce: nonce_str,
-                        res: res_sender,
-                    })
-                    .await
-                    .unwrap();
-
-                // Receive the response
-                let response = res_receiver.recv().await.unwrap().expect("Failed to receive response");
-                eprintln!("response: {:?}", response);
-            }
-            let job_message_content = "describe the image".to_string();
+            let job_message_content = "tell me a joke".to_string();
             {
                 // Send a Message to the Job for processing
                 eprintln!("\n\nSend a message for the Job");
@@ -224,7 +170,7 @@ fn job_image_analysis() {
                     &agent_subidentity.clone(),
                     &job_id.clone().to_string(),
                     &job_message_content,
-                    &hash_of_aes_encryption_key_hex(symmetrical_sk),
+                    "",
                 )
                 .await;
 
@@ -233,11 +179,13 @@ fn job_image_analysis() {
             }
             {
                 eprintln!("Waiting for the Job to finish");
-                for _ in 0..50 {
+                tokio::time::sleep(Duration::from_secs(2)).await;
+                let mut job_completed = false;
+                for _ in 0..5 {
                     let (res1_sender, res1_receiver) = async_channel::bounded(1);
                     node1_commands_sender
                         .send(NodeCommand::FetchLastMessages {
-                            limit: 2,
+                            limit: 4,
                             res: res1_sender,
                         })
                         .await
@@ -245,25 +193,85 @@ fn job_image_analysis() {
                     let node1_last_messages = res1_receiver.recv().await.unwrap();
                     eprintln!("node1_last_messages: {:?}", node1_last_messages);
 
-                    match node1_last_messages[0].get_message_content() {
-                        Ok(message_content) => match serde_json::from_str::<JobMessage>(&message_content) {
-                            Ok(job_message) => {
-                                // eprintln!("message_content: {}", message_content);
-                                if job_message.content != job_message_content {
-                                    assert!(true);
+                    if node1_last_messages.len() >= 2 {
+                        match node1_last_messages[1].get_message_content() {
+                            Ok(message_content) => match serde_json::from_str::<JobMessage>(&message_content) {
+                                Ok(job_message) => {
+                                    eprintln!("message_content: {}", job_message.content);
+                                    job_completed = true;
                                     break;
                                 }
-                            }
+                                Err(_) => {
+                                    eprintln!("error: message_content: {}", message_content);
+                                }
+                            },
                             Err(_) => {
-                                eprintln!("error: message_content: {}", message_content);
+                                // nothing
                             }
-                        },
-                        Err(_) => {
-                            // nothing
                         }
                     }
-                    tokio::time::sleep(Duration::from_secs(10)).await;
+                    tokio::time::sleep(Duration::from_millis(500)).await;
                 }
+                assert!(job_completed, "Job did not complete within the expected time");
+            }
+            let second_job_message_content = "I didn't understand the joke. Can you explain it?".to_string();
+            {
+                // Sending a second message to the Job for processing
+                eprintln!("\n\nSend a second message for the Job");
+                let start = Instant::now();
+                api_message_job(
+                    node1_commands_sender.clone(),
+                    clone_static_secret_key(&node1_profile_encryption_sk),
+                    node1_encryption_pk.clone(),
+                    clone_signature_secret_key(&node1_profile_identity_sk),
+                    node1_identity_name.clone().as_str(),
+                    node1_profile_name.clone().as_str(),
+                    &agent_subidentity.clone(),
+                    &job_id.clone().to_string(),
+                    &second_job_message_content,
+                    "",
+                )
+                .await;
+
+                let duration = start.elapsed(); // Get the time elapsed since the start of the timer
+                eprintln!("Time elapsed in api_message_job is: {:?}", duration);
+            }
+            {
+                eprintln!("Waiting for the Job to finish");
+                tokio::time::sleep(Duration::from_secs(2)).await;
+                let mut job_completed = false;
+                for _ in 0..5 {
+                    let (res1_sender, res1_receiver) = async_channel::bounded(1);
+                    node1_commands_sender
+                        .send(NodeCommand::FetchLastMessages {
+                            limit: 4,
+                            res: res1_sender,
+                        })
+                        .await
+                        .unwrap();
+                    let node1_last_messages = res1_receiver.recv().await.unwrap();
+                    eprintln!("node1_last_messages: {:?}", node1_last_messages);
+
+                    if node1_last_messages.len() >= 4 {
+                        match node1_last_messages[3].get_message_content() {
+                            Ok(message_content) => match serde_json::from_str::<JobMessage>(&message_content) {
+                                Ok(job_message) => {
+                                    eprintln!("message_content: {}", job_message.content);
+                                    job_completed = true;
+                                    break;
+                                }
+                                Err(_) => {
+                                    eprintln!("error: message_content: {}", message_content);
+                                }
+                            },
+                            Err(_) => {
+                                // nothing
+                            }
+                        }
+                    }
+                    tokio::time::sleep(Duration::from_millis(500)).await;
+                }
+                assert!(job_completed, "Job did not complete within the expected time");
             }
         })
     });
