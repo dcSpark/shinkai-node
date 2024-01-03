6e2aa63033381bc75f9715c6db7fd5bdb2247cb5
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2024-01-03 12:41:25 +0100

    Added source file reading

--- src/vector_fs/vector_fs_reader.rs
+++ src/vector_fs/vector_fs_reader.rs
@@ -3,6 +3,7 @@ use super::vector_fs_error::VectorFSError;
 use super::vector_fs_types::{FSEntry, FSFolder, FSItem};
 use shinkai_message_primitives::schemas::shinkai_name::ShinkaiName;
 use shinkai_vector_resources::resource_errors::VRError;
+use shinkai_vector_resources::source::SourceFile;
 use shinkai_vector_resources::vector_resource::{
     BaseVectorResource, NodeContent, VectorResource, VectorResourceCore, VectorResourceSearch,
 };
@@ -45,11 +46,20 @@ impl<'a> VFSReader<'a> {
         Ok(reader)
     }
 
-    /// Retrieves the FSEntry for the path in the VectorFS
+    /// Retrieves the FSEntry for the path in the VectorFS. If path is root `/`, then returns a
+    /// FSFolder that matches the FS root structure.
     pub fn retrieve_fs_entry(&self) -> Result<FSEntry, VectorFSError> {
         let internals = self.vector_fs._get_profile_fs_internals_read_only(&self.profile)?;
-        let ret_node = internals.fs_core_resource.retrieve_node_at_path(self.path.clone())?;
 
+        // Create FSFolder directly if path is root
+        if self.path.is_empty() {
+            let core_resource = BaseVectorResource::Map(internals.fs_core_resource.clone());
+            let fs_folder = FSFolder::from_vector_resource(core_resource, self.path.clone())?;
+            return Ok(FSEntry::Folder(fs_folder));
+        }
+
+        // Otherwise retrieve the node and process it
+        let ret_node = internals.fs_core_resource.retrieve_node_at_path(self.path.clone())?;
         match ret_node.node.content {
             NodeContent::Resource(_) => {
                 let fs_folder = FSFolder::from_vector_resource_node(ret_node.node.clone(), self.path.clone())?;
@@ -68,5 +78,20 @@ impl<'a> VFSReader<'a> {
     pub fn retrieve_vector_resource(&self) -> Result<BaseVectorResource, VectorFSError> {
         let fs_item = self.retrieve_fs_entry()?.as_item()?;
         self.vector_fs.db.get_resource_by_fs_item(&fs_item, &self.profile)
+    }
+
+    /// Attempts to retrieve a SourceFile at the path. If this path does not currently exist, or
+    /// a source_file is not saved at this path, then an error is returned.
+    pub fn retrieve_source_file(&self) -> Result<SourceFile, VectorFSError> {
+        let fs_item = self.retrieve_fs_entry()?.as_item()?;
+        self.vector_fs.db.get_source_file_by_fs_item(&fs_item, &self.profile)
+    }
+
+    /// Attempts to retrieve a VectorResource and its SourceFile at the path. If either is not available
+    /// an error will be returned.
+    pub fn retrieve_vr_and_source_file(&self) -> Result<(BaseVectorResource, SourceFile), VectorFSError> {
+        let vr = self.retrieve_vector_resource()?;
+        let sf = self.retrieve_source_file()?;
+        Ok((vr, sf))
     }
 }
