528c2afb0b41c9cb61be3ed35317fc11791cc46b
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-02-16 00:15:48 -0600

    missing

--- src/network/node.rs
+++ src/network/node.rs
@@ -276,9 +276,20 @@ pub enum NodeCommand {
         msg: ShinkaiMessage,
         res: Sender<Result<String, APIError>>,
     },
+    APIVecFSRetrieveVectorResource {
+        // Missing: retrieve_vector_resource(reader: &VFSReader) -> Result<BaseVectorResource, VectorFSError>
+        msg: ShinkaiMessage,
+        res: Sender<Result<String, APIError>>,
+    },
     APIVecFSRetrieveVectorSearchSimplifiedJson {
         msg: ShinkaiMessage,
         res: Sender<Result<Vec<String>, APIError>>,
+    },
+    APISaveVectorResourceInFolder {
+        // Missing: save_vector_resource_in_folder(writer: &VFSWriter, resource: BaseVectorResource, source_file_map: Option<SourceFileMap>, distribution_origin: DistributionOrigin) -> Result<FSItem, VectorFSError>
+        // .vrkai? so it needs a file inbox with one file that's a vrkai
+        msg: ShinkaiMessage,
+        res: Sender<Result<String, APIError>>,
     },
     APIVecFSCreateFolder {
         msg: ShinkaiMessage,
