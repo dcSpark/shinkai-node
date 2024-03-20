c9b85bd07abe95141efdd06b1266d5e08c3f239a
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-03-20 11:13:40 -0500

    update

--- src/schemas/smart_inbox.rs
+++ src/schemas/smart_inbox.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use shinkai_message_primitives::shinkai_message::shinkai_message::ShinkaiMessage;
+use shinkai_message_primitives::{shinkai_message::shinkai_message::ShinkaiMessage, shinkai_utils::job_scope::JobScope};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SmartInbox {
@@ -7,4 +7,5 @@ pub struct SmartInbox {
     pub custom_name: String,
     pub last_message: Option<ShinkaiMessage>,
     pub is_finished: bool,
+    pub job_scope: Option<JobScope>,
 }
