94452791b0c4b0c1c495488da7b2ba354fd617e8
Alfredo Gallardo <agallardol@outlook.com>
2024-03-19 22:35:52 -0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // main.rs
 #![recursion_limit = "256"]
+
 mod agent;
 mod cron_tasks;
 mod crypto_identities;
@@ -16,8 +17,35 @@ mod runner;
 
 use runner::{initialize_node, run_node_tasks};
 
-#[tokio::main]
+use std::thread;
+use std::time::Duration;
+use parking_lot::deadlock;
+
+extern crate core_affinity;
+
+#[tokio::main(flavor = "current_thread")]
 pub async fn main() {
+// Create a background thread which checks for deadlocks every 10s
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_secs(1));
+            let deadlocks = deadlock::check_deadlock();
+            if deadlocks.is_empty() {
+                println!("No deadlocks found");
+                continue;
+            }
+
+            println!("{} deadlocks detected", deadlocks.len());
+            for (i, threads) in deadlocks.iter().enumerate() {
+                println!("Deadlock #{}", i);
+                for t in threads {
+                    println!("Thread Id {:#?}", t.thread_id());
+                    println!("{:#?}", t.backtrace());
+                }
+            }
+        }
+    });
+    console_subscriber::init();
     let result = initialize_node().await.unwrap();
     let _ = run_node_tasks(result.1, result.2, result.3, result.4).await;
 }
