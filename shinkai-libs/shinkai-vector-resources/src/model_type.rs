fa7daf0a3eb2b3da04bd7c4b4c98ab2f00c6a241
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2024-04-03 00:01:35 +0200

    Added commits to all models/improved string methods

--- shinkai-libs/shinkai-vector-resources/src/model_type.rs
+++ shinkai-libs/shinkai-vector-resources/src/model_type.rs
@@ -85,39 +85,62 @@ pub enum TextEmbeddingsInference {
     Other(String),
 }
 impl TextEmbeddingsInference {
-    const ALL_MINI_LML6V2: &'static str = "hftei/sentence-transformers/all-MiniLM-L6-v2";
-    const ALL_MINI_LML12V2: &'static str = "hftei/sentence-transformers/all-MiniLM-L12-v2";
-    const MULTI_QA_MINI_LML6: &'static str = "hftei/sentence-transformers/multi-qa-MiniLM-L6-cos-v1";
-    const BGE_LARGE_ENV1_5: &'static str = "hftei/BAAI/bge-large-en-v1.5";
-    const BGE_BASE_EN1_5: &'static str = "hftei/BAAI/bge-base-en-v1.5";
-    const BGE_SMALL_EN1_5: &'static str = "hftei/BAAI/bge-small-en-v1.5";
-    const EMBER_V1: &'static str = "hftei/llmrails/ember-v1";
-    const GTE_LARGE: &'static str = "hftei/thenlper/gte-large";
-    const GTE_BASE: &'static str = "hftei/thenlper/gte-base";
-    const E5_LARGE_V2: &'static str = "hftei/intfloat/e5-large-v2";
-    const E5_BASE_V2: &'static str = "hftei/intfloat/e5-base-v2";
-    const MULTILINGUAL_E5_LARGE: &'static str = "hftei/intfloat/multilingual-e5-large";
+    const ALL_MINI_LML6V2: &'static str =
+        "sentence-transformers/all-MiniLM-L6-v2#0b6dc4ef7c29dba0d2e99a5db0c855c3102310d8";
+    const ALL_MINI_LML12V2: &'static str =
+        "sentence-transformers/all-MiniLM-L12-v2#a05860a77cef7b37e0048a7864658139bc18a854";
+    const MULTI_QA_MINI_LML6: &'static str =
+        "sentence-transformers/multi-qa-MiniLM-L6-cos-v1#2430568290bb832d22ad5064f44dd86cf0240142";
+    const BGE_LARGE_ENV1_5: &'static str = "BAAI/bge-large-en-v1.5#d4aa6901d3a41ba39fb536a557fa166f842b0e09";
+    const BGE_BASE_EN1_5: &'static str = "BAAI/bge-base-en-v1.5#a5beb1e3e68b9ab74eb54cfd186867f64f240e1a";
+    const BGE_SMALL_EN1_5: &'static str = "BAAI/bge-small-en-v1.5#5c38ec7c405ec4b44b94cc5a9bb96e735b38267a";
+    const EMBER_V1: &'static str = "llmrails/ember-v1#9e76885bed0dcfa38cbf01e1e27b3a0e8d36d4e4";
+    const GTE_LARGE: &'static str = "thenlper/gte-large#58578616559541da766b9b993734f63bcfcfc057";
+    const GTE_BASE: &'static str = "thenlper/gte-base#5e95d41db6721e7cbd5006e99c7508f0083223d6";
+    const E5_LARGE_V2: &'static str = "intfloat/e5-large-v2#b322e09026e4ea05f42beadf4d661fb4e101d311";
+    const E5_BASE_V2: &'static str = "intfloat/e5-base-v2#1c644c92ad3ba1efdad3f1451a637716616a20e8";
+    const MULTILINGUAL_E5_LARGE: &'static str =
+        "intfloat/multilingual-e5-large#ab10c1a7f42e74530fe7ae5be82e6d4f11a719eb";
 
+    pub const SUPPORTED_MODELS: [&'static str; 12] = [
+        Self::ALL_MINI_LML6V2,
+        Self::ALL_MINI_LML12V2,
+        Self::MULTI_QA_MINI_LML6,
+        Self::BGE_LARGE_ENV1_5,
+        Self::BGE_BASE_EN1_5,
+        Self::BGE_SMALL_EN1_5,
+        Self::EMBER_V1,
+        Self::GTE_LARGE,
+        Self::GTE_BASE,
+        Self::E5_LARGE_V2,
+        Self::E5_BASE_V2,
+        Self::MULTILINGUAL_E5_LARGE,
+    ];
+
+    /// Returns the model name + commit in the format of "hftei/<model>#<commit>"
     fn to_string(&self) -> String {
-        match self {
-            TextEmbeddingsInference::AllMiniLML6v2 => Self::ALL_MINI_LML6V2.to_string(),
-            TextEmbeddingsInference::AllMiniLML12v2 => Self::ALL_MINI_LML12V2.to_string(),
-            TextEmbeddingsInference::MultiQAMiniLML6 => Self::MULTI_QA_MINI_LML6.to_string(),
-            TextEmbeddingsInference::BgeLargeEnv1_5 => Self::BGE_LARGE_ENV1_5.to_string(),
-            TextEmbeddingsInference::BgeBaseEn1_5 => Self::BGE_BASE_EN1_5.to_string(),
-            TextEmbeddingsInference::BgeSmallEn1_5 => Self::BGE_SMALL_EN1_5.to_string(),
-            TextEmbeddingsInference::EmberV1 => Self::EMBER_V1.to_string(),
-            TextEmbeddingsInference::GteLarge => Self::GTE_LARGE.to_string(),
-            TextEmbeddingsInference::GteBase => Self::GTE_BASE.to_string(),
-            TextEmbeddingsInference::E5LargeV2 => Self::E5_LARGE_V2.to_string(),
-            TextEmbeddingsInference::E5BaseV2 => Self::E5_BASE_V2.to_string(),
-            TextEmbeddingsInference::MultilingualE5Large => Self::MULTILINGUAL_E5_LARGE.to_string(),
-            TextEmbeddingsInference::Other(name) => format!("hftei/{}", name),
-        }
+        let model_str = match self {
+            TextEmbeddingsInference::AllMiniLML6v2 => Self::ALL_MINI_LML6V2,
+            TextEmbeddingsInference::AllMiniLML12v2 => Self::ALL_MINI_LML12V2,
+            TextEmbeddingsInference::MultiQAMiniLML6 => Self::MULTI_QA_MINI_LML6,
+            TextEmbeddingsInference::BgeLargeEnv1_5 => Self::BGE_LARGE_ENV1_5,
+            TextEmbeddingsInference::BgeBaseEn1_5 => Self::BGE_BASE_EN1_5,
+            TextEmbeddingsInference::BgeSmallEn1_5 => Self::BGE_SMALL_EN1_5,
+            TextEmbeddingsInference::EmberV1 => Self::EMBER_V1,
+            TextEmbeddingsInference::GteLarge => Self::GTE_LARGE,
+            TextEmbeddingsInference::GteBase => Self::GTE_BASE,
+            TextEmbeddingsInference::E5LargeV2 => Self::E5_LARGE_V2,
+            TextEmbeddingsInference::E5BaseV2 => Self::E5_BASE_V2,
+            TextEmbeddingsInference::MultilingualE5Large => Self::MULTILINGUAL_E5_LARGE,
+            TextEmbeddingsInference::Other(name) => name,
+        };
+        format!("hftei/{}", model_str)
     }
 
+    /// Parses a string in the format of "hftei/<model>#<commit>" into a TextEmbeddingsInference
     fn from_string(s: &str) -> Result<Self, VRError> {
-        match s {
+        let stripped = s.strip_prefix("hftei/").ok_or(VRError::InvalidModelArchitecture)?;
+        match stripped {
             Self::ALL_MINI_LML6V2 => Ok(TextEmbeddingsInference::AllMiniLML6v2),
             Self::ALL_MINI_LML12V2 => Ok(TextEmbeddingsInference::AllMiniLML12v2),
             Self::MULTI_QA_MINI_LML6 => Ok(TextEmbeddingsInference::MultiQAMiniLML6),
@@ -130,7 +153,7 @@ impl TextEmbeddingsInference {
             Self::E5_LARGE_V2 => Ok(TextEmbeddingsInference::E5LargeV2),
             Self::E5_BASE_V2 => Ok(TextEmbeddingsInference::E5BaseV2),
             Self::MULTILINGUAL_E5_LARGE => Ok(TextEmbeddingsInference::MultilingualE5Large),
-            _ => Err(VRError::InvalidModelArchitecture),
+            _ => Ok(TextEmbeddingsInference::Other(stripped.to_string())),
         }
     }
 }
