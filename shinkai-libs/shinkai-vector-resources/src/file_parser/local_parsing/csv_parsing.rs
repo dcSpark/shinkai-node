c7862090dceeda2dded9b588c216f8600833eb27
benolt <bence.oltean@gmail.com>
2024-04-24 14:02:59 +0200

    fix import

--- shinkai-libs/shinkai-vector-resources/src/file_parser/local_parsing/csv_parsing.rs
+++ shinkai-libs/shinkai-vector-resources/src/file_parser/local_parsing/csv_parsing.rs
@@ -4,7 +4,7 @@ use crate::{
     resource_errors::VRError,
 };
 use csv::ReaderBuilder;
-use std::io::Cursor;
+use std::{collections::HashMap, io::Cursor};
 
 impl LocalFileParser {
     /// Attempts to process the provided csv file into a list of TextGroups.
