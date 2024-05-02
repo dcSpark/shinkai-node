15b45f8b5c46d55bc1a5b58730ac7f7e5cb3724c
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-05-01 21:04:37 -0500

    update

--- scripts/run_node1.sh
+++ scripts/run_node1.sh
@@ -14,7 +14,7 @@ export STARTING_NUM_QR_DEVICES="1"
 export FIRST_DEVICE_NEEDS_REGISTRATION_CODE="false"
 export LOG_SIMPLE="true"
 export NO_SECRET_FILE="true"
-export EMBEDDINGS_SERVER_URL="https://public.shinkai.com/x-em"
+export EMBEDDINGS_SERVER_URL="http://localhost:11434"
 export UNSTRUCTURED_SERVER_URL="https://public.shinkai.com/x-un"
 
 # export TELEMETRY_ENDPOINT="https://apm-node-b1.shinkai.com/api/default"
