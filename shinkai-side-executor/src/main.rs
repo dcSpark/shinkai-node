2505342cdae3d50b90a4b6ede076fab04d90d96a
benolt <bence.oltean@gmail.com>
2024-05-15 16:25:13 +0200

    PDF text parsing, extracting images

--- shinkai-side-executor/src/main.rs
+++ shinkai-side-executor/src/main.rs
@@ -2,61 +2,65 @@ use image::ImageFormat;
 use pdfium_render::prelude::*;
 
 pub fn main() -> Result<(), PdfiumError> {
-    // Attempt to bind to a pdfium library in the current working directory; failing that,
-    // attempt to bind to a system-provided library.
-
-    // The library name will differ depending on the current platform. On Linux,
-    // the library will be named libpdfium.so by default; on Windows, pdfium.dll; and on
-    // MacOS, libpdfium.dylib. We can use the Pdfium::pdfium_platform_library_name_at_path()
-    // function to append the correct library name for the current platform to a path we specify.
-
-    // === Dynamic linking ===
-    // let bindings = Pdfium::bind_to_library(
-    //     // Attempt to bind to a pdfium library in the current working directory...
-    //     Pdfium::pdfium_platform_library_name_at_path("./lib"),
-    // )?;
-
-    // let pdfium = Pdfium::new(bindings);
-
-    // This pattern is common enough that it is the default constructor for the Pdfium struct,
-    // so we could have also simply written:
-
-    // let pdfium = Pdfium::default();
-
     // === Static linking ===
     // PDFIUM_STATIC_LIB_PATH="/path-to/shinkai-node/shinkai-side-executor/lib" cargo build
     let pdfium = Pdfium::new(Pdfium::bind_to_statically_linked_library().unwrap());
 
-    // Next, we create a set of shared settings that we'll apply to each page in the
-    // sample file when rendering. Sharing the same rendering configuration is a good way
-    // to ensure homogenous output across all pages in the document.
-
-    let render_config = PdfRenderConfig::new()
-        .set_target_width(2000)
-        .set_maximum_height(2000)
-        .rotate_if_landscape(PdfPageRenderRotation::Degrees90, true);
-
-    // Load the sample file...
-
     let document = pdfium.load_pdf_from_file("../files/Shinkai_Protocol_Whitepaper.pdf", None)?;
 
-    // ... and export each page to a JPEG in the current working directory,
-    // using the rendering configuration we created above.
+    let mut image_counter = 0u64;
 
-    for (index, page) in document
-        .pages()
-        .iter() // ... get an iterator across all pages ...
-        .enumerate()
-    {
-        println!("Exporting page {}", index + 1);
-        let result = page
-            .render_with_config(&render_config)? // Initializes a bitmap with the given configuration for this page ...
-            .as_image() // ... renders it to an Image::DynamicImage ...
-            .as_rgba8() // ... sets the correct color space ...
-            .ok_or(PdfiumError::ImageError)?
-            .save_with_format(format!("exported-page-{}.png", index + 1), ImageFormat::Png); // ... and exports it to a JPEG.
+    for (page_index, page) in document.pages().iter().enumerate() {
+        println!("=============== Page {} ===============", page_index + 1);
 
-        assert!(result.is_ok());
+        println!("-=-=-=- Objects -=-=-=-");
+        for object in page.objects().iter() {
+            match object.object_type() {
+                PdfPageObjectType::Text => {
+                    let text_object = object.as_text_object().unwrap();
+                    println!(
+                        "Text object: [{} {:?}] {:?}",
+                        text_object.unscaled_font_size().value,
+                        text_object.font().weight()?,
+                        text_object.text()
+                    );
+                }
+                PdfPageObjectType::Image => {
+                    let image_object = object.as_image_object().unwrap();
+                    println!(
+                        "Image object: {} x {}",
+                        image_object.width()?.value,
+                        image_object.height()?.value
+                    );
+
+                    image_counter += 1;
+
+                    let image_name = format!("image-{:#03}-page-{}.png", image_counter, page_index + 1);
+
+                    let result = image_object
+                        .get_raw_image()?
+                        .save_with_format(&image_name, ImageFormat::Png);
+
+                    println!("Image object saved: {:?} {}", result, &image_name);
+                }
+                PdfPageObjectType::Path => {
+                    let path_object = object.as_path_object().unwrap();
+                    println!("Path object: {:?}", path_object.bounds());
+                }
+                object_type => {
+                    println!("Object type {:?}", object_type);
+                }
+            }
+        }
+
+        println!("-=-=-=- Annotations -=-=-=-");
+        for annotation in page.annotations().iter() {
+            println!(
+                "Annotation type: {:?}, text: {:?}",
+                annotation.annotation_type(),
+                page.text().unwrap().for_annotation(&annotation).ok()
+            );
+        }
     }
 
     Ok(())
