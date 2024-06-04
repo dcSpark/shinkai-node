460e012fb960b2b53a78b9b6167abffb68d29b9b
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-06-03 21:05:03 -0500

    fix another test

--- shinkai-bin/shinkai-node/tests/it/agent_integration_tests.rs
+++ shinkai-bin/shinkai-node/tests/it/agent_integration_tests.rs
@@ -26,7 +26,7 @@ use std::{net::SocketAddr, time::Duration};
 use tokio::runtime::Runtime;
 
 use super::utils::node_test_api::{
-    api_agent_registration, api_create_job, api_message_job, api_registration_device_node_profile_main,
+    api_create_job, api_message_job, api_registration_device_node_profile_main,
 };
 
 use mockito::Server;
