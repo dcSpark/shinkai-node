de26546f7c7cb7f213c61baf844364f140560edd
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-06-06 18:09:43 -0500

    fix localhost script

--- scripts/run_node_localhost.sh
+++ scripts/run_node_localhost.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export NODE_IP="0.0.0.0"
-export NODE_PORT="9552"
+export NODE_PORT="9452"
 export NODE_API_IP="0.0.0.0"
-export NODE_API_PORT="9550"
+export NODE_API_PORT="9450"
 export PING_INTERVAL_SECS="0"
 export GLOBAL_IDENTITY_NAME="@@localhost.arb-sep-shinkai"
 export RUST_LOG=debug,error,info
@@ -11,7 +11,6 @@ export STARTING_NUM_QR_PROFILES="1"
 export STARTING_NUM_QR_DEVICES="1"
 export FIRST_DEVICE_NEEDS_REGISTRATION_CODE="false"
 export LOG_SIMPLE="true"
-export NO_SECRET_FILE="true"
 export EMBEDDINGS_SERVER_URL="http://localhost:11434" # assumes that you installed the embeddings server locally using ollama (shinkai-apps helps you handling all of this)
 # export EMBEDDINGS_SERVER_URL="https://public.shinkai.com/x-em" # if you prefer to use the public embeddings server
 export UNSTRUCTURED_SERVER_URL="https://public.shinkai.com/x-un" # we are replacing unstructure soon if you prefer it to install it locally https://docs.shinkai.com/getting-started#unstructured-api
