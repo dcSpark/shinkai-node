bfbfbc5055f3ae30f40351abef8a5534b16c6c7e
benolt <bence.oltean@gmail.com>
2024-06-18 10:54:14 +0200

    update lib path env var

--- .github/run-main-cargo-tests.sh
+++ .github/run-main-cargo-tests.sh
@@ -2,5 +2,5 @@
 
 export IS_TESTING=1
 export WELCOME_MESSAGE=false
-export PDFIUM_DYNAMIC_LIB_PATH=/app/shinkai-bin/shinkai-executor/pdfium/linux-x64/libpdfium.so
+export PDFIUM_DYNAMIC_LIB_PATH=/app/shinkai-bin/shinkai-executor/pdfium/linux-x64
 cd /app && cargo test -- --test-threads=1
