1448ab22f5aa8bf233880e4e04703b687068d6ee
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-07-02 19:17:15 -0500

    update

--- scripts/run_node_pdf_parser.sh
+++ scripts/run_node_pdf_parser.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export NODE_IP="0.0.0.0"
-export NODE_PORT="9552"
+export NODE_PORT="9652"
 export NODE_API_IP="0.0.0.0"
-export NODE_API_PORT="9550"
+export NODE_API_PORT="9650"
 export IDENTITY_SECRET_KEY="df3f619804a92fdb4057192dc43dd748ea778adc52bc498ce80524c014b81119"
 export ENCRYPTION_SECRET_KEY="d83f619804a92fdb4057192dc43dd748ea778adc52bc498ce80524c014b81159"
 export PING_INTERVAL_SECS="0"
@@ -17,10 +17,7 @@ export NO_SECRET_FILE="true"
 export EMBEDDINGS_SERVER_URL="https://internal.shinkai.com/x-embed-api/"
 export UNSTRUCTURED_SERVER_URL="https://internal.shinkai.com/x-unstructured-api/"
 export NODE_STORAGE_PATH="storage"
-
-export STATIC_SERVER_PORT="9554"
-export STATIC_SERVER_IP="0.0.0.0"
-export STATIC_SERVER_FOLDER="./static_server_example"
+export DEBUG_VRKAI="1"
 
 export INITIAL_AGENT_NAMES="llama3_8b"
 export INITIAL_AGENT_URLS="http://localhost:11434"
