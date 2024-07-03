4d65470f948c4d69b76f7941a98abc50ce480e91
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-07-03 18:44:16 -0500

    add test file fix warning

--- shinkai-libs/shinkai-ocr/tests/image_parser_tests.rs
+++ shinkai-libs/shinkai-ocr/tests/image_parser_tests.rs
@@ -7,7 +7,7 @@ async fn image_parsing() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::read("../../files/product_table.png")?;
     let image_parser = ImageParser::new()?;
     let parsed_text = image_parser.process_image_file(file)?;
-    let table = parsed_text.split("\n").collect::<Vec<_>>();
+    let table = parsed_text.split('\n').collect::<Vec<_>>();
 
     assert!(table[1].contains("Product"));
     assert!(table[2].contains("Chocolade"));
