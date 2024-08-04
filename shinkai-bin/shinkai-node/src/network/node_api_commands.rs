df72a9b1d91fc36620657b16f86cefb9c867a4ce
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-08-03 23:58:02 -0500

    fix list workflows bug

--- shinkai-bin/shinkai-node/src/network/node_api_commands.rs
+++ shinkai-bin/shinkai-node/src/network/node_api_commands.rs
@@ -3275,7 +3275,10 @@ impl Node {
         }
     }
 
+    #[allow(clippy::too_many_arguments)]
     pub async fn api_list_all_workflows(
+        tool_router: Option<Arc<Mutex<ToolRouter>>>,
+        generator: Arc<RemoteEmbeddingGenerator>,
         db: Arc<ShinkaiDB>,
         node_name: ShinkaiName,
         identity_manager: Arc<Mutex<IdentityManager>>,
@@ -3308,6 +3311,29 @@ impl Node {
             };
             let _ = res.send(Err(api_error)).await;
             return Ok(());
+        }
+
+        // Check if tool_router is available and has started if not start it
+        if let Some(tool_router) = &tool_router {
+            let mut tool_router = tool_router.lock().await;
+            if !tool_router.is_started() {
+                if let Err(err) = tool_router
+                    .start(
+                        Box::new((*generator).clone()),
+                        Arc::downgrade(&db),
+                        requester_name.clone(),
+                    )
+                    .await
+                {
+                    let api_error = APIError {
+                        code: StatusCode::INTERNAL_SERVER_ERROR.as_u16(),
+                        error: "Internal Server Error".to_string(),
+                        message: format!("Failed to start tool router: {}", err),
+                    };
+                    let _ = res.send(Err(api_error)).await;
+                    return Ok(());
+                }
+            }
         }
 
         // List all workflows for the user
