88ecbcc41e261b10d3b2a81e4f621eb3d7c7183b
benolt <bence.oltean@gmail.com>
2024-08-13 15:08:22 +0200

    read indexer entities and reports improvements, compute community weights

--- shinkai-libs/shinkai-graphrag/src/context_builder/indexer_reports.rs
+++ shinkai-libs/shinkai-graphrag/src/context_builder/indexer_reports.rs
@@ -1,8 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use polars::prelude::*;
 use polars_lazy::dsl::col;
 use serde::{Deserialize, Serialize};
+
+use super::indexer_entities::get_field;
 
 pub fn read_indexer_reports(
     final_community_reports: &DataFrame,
@@ -12,33 +14,13 @@ pub fn read_indexer_reports(
     let entity_df = final_nodes.clone();
     let entity_df = filter_under_community_level(&entity_df, community_level)?;
 
-    let entity_df = entity_df
-        .clone()
+    let filtered_community_df = entity_df
         .lazy()
         .with_column(col("community").fill_null(lit(-1)))
-        .collect()?;
-    let entity_df = entity_df
-        .clone()
-        .lazy()
         .with_column(col("community").cast(DataType::Int32))
-        .collect()?;
-
-    let entity_df = entity_df
-        .clone()
-        .lazy()
-        .with_column(col("community").cast(DataType::String))
-        .collect()?;
-
-    let entity_df = entity_df
-        .clone()
-        .lazy()
         .group_by([col("title")])
         .agg([col("community").max()])
-        .collect()?;
-
-    let filtered_community_df = entity_df
-        .clone()
-        .lazy()
+        .with_column(col("community").cast(DataType::String))
         .filter(len().over([col("community")]).gt(lit(1)))
         .collect()?;
 
@@ -46,7 +28,6 @@ pub fn read_indexer_reports(
     let report_df = filter_under_community_level(&report_df, community_level)?;
 
     let report_df = report_df
-        .clone()
         .lazy()
         .join(
             filtered_community_df.clone().lazy(),
@@ -56,7 +37,18 @@ pub fn read_indexer_reports(
         )
         .collect()?;
 
-    let reports = read_community_reports(&report_df, "community", Some("community"), None, None)?;
+    let reports = read_community_reports(
+        &report_df,
+        "community",
+        Some("community"),
+        "title",
+        "community",
+        "summary",
+        "full_content",
+        Some("rank"),
+        None,
+        None,
+    )?;
     Ok(reports)
 }
 
@@ -83,21 +75,36 @@ pub struct CommunityReport {
 
 pub fn read_community_reports(
     df: &DataFrame,
-    _id_col: &str,
-    _short_id_col: Option<&str>,
-    // title_col: &str,
-    // community_col: &str,
-    // summary_col: &str,
-    // content_col: &str,
-    // rank_col: Option<&str>,
+    id_col: &str,
+    short_id_col: Option<&str>,
+    title_col: &str,
+    community_col: &str,
+    summary_col: &str,
+    content_col: &str,
+    rank_col: Option<&str>,
     _summary_embedding_col: Option<&str>,
     _content_embedding_col: Option<&str>,
     // attributes_cols: Option<&[&str]>,
 ) -> anyhow::Result<Vec<CommunityReport>> {
+    let column_names = [
+        Some(id_col),
+        short_id_col,
+        Some(title_col),
+        Some(community_col),
+        Some(summary_col),
+        Some(content_col),
+        rank_col,
+    ]
+    .iter()
+    .filter_map(|&v| v.map(|v| v.to_string()))
+    .collect::<Vec<_>>();
+
+    let column_names: Vec<String> = column_names.into_iter().collect::<HashSet<String>>().into_vec();
+
     let mut df = df.clone();
     df.as_single_chunk_par();
     let mut iters = df
-        .columns(["community", "title", "summary", "full_content", "rank"])?
+        .columns(column_names.clone())?
         .iter()
         .map(|s| s.iter())
         .collect::<Vec<_>>();
@@ -108,22 +115,42 @@ pub fn read_community_reports(
         for iter in &mut iters {
             let value = iter.next();
             if let Some(value) = value {
-                row_values.push(value.to_string());
+                row_values.push(value);
             }
         }
         rows.push(row_values);
     }
 
     let mut reports = Vec::new();
-    for row in rows {
+    for (idx, row) in rows.iter().enumerate() {
         let report = CommunityReport {
-            id: row.get(0).unwrap_or(&String::new()).to_string(),
-            short_id: Some(row.get(0).unwrap_or(&String::new()).to_string()),
-            title: row.get(1).unwrap_or(&String::new()).to_string(),
-            community_id: row.get(0).unwrap_or(&String::new()).to_string(),
-            summary: row.get(2).unwrap_or(&String::new()).to_string(),
-            full_content: row.get(3).unwrap_or(&String::new()).to_string(),
-            rank: Some(row.get(4).and_then(|v| v.parse::<f64>().ok()).unwrap_or(0.0)),
+            id: get_field(&row, id_col, &column_names)
+                .map(|id| id.to_string())
+                .unwrap_or(String::new()),
+            short_id: Some(
+                short_id_col
+                    .map(|short_id| get_field(&row, short_id, &column_names))
+                    .flatten()
+                    .map(|short_id| short_id.to_string())
+                    .unwrap_or(idx.to_string()),
+            ),
+            title: get_field(&row, title_col, &column_names)
+                .map(|title| title.to_string())
+                .unwrap_or(String::new()),
+            community_id: get_field(&row, community_col, &column_names)
+                .map(|community| community.to_string())
+                .unwrap_or(String::new()),
+            summary: get_field(&row, summary_col, &column_names)
+                .map(|summary| summary.to_string())
+                .unwrap_or(String::new()),
+            full_content: get_field(&row, content_col, &column_names)
+                .map(|content| content.to_string())
+                .unwrap_or(String::new()),
+            rank: rank_col
+                .map(|rank_col| {
+                    get_field(&row, rank_col, &column_names).map(|v| v.to_string().parse::<f64>().unwrap_or(0.0))
+                })
+                .flatten(),
             summary_embedding: None,
             full_content_embedding: None,
             attributes: None,
@@ -131,5 +158,15 @@ pub fn read_community_reports(
         reports.push(report);
     }
 
-    Ok(reports)
+    let mut unique_reports: Vec<CommunityReport> = Vec::new();
+    let mut report_ids: HashSet<String> = HashSet::new();
+
+    for report in reports {
+        if !report_ids.contains(&report.id) {
+            unique_reports.push(report.clone());
+            report_ids.insert(report.id);
+        }
+    }
+
+    Ok(unique_reports)
 }
