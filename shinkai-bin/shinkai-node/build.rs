3e7187f04ff26e5abe1c954735f6dfc5f79bc258
Alfredo Gallardo <agallardol@outlook.com>
2024-08-29 17:05:34 -0400

    - fix: shinkai tools backend location

--- shinkai-bin/shinkai-node/build.rs
+++ shinkai-bin/shinkai-node/build.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 fn main() {
     shinkai_tools_runner::copy_assets::copy_assets(
-        "0.7.4",
+        "0.7.5",
         Some(PathBuf::from("../../")),
         Some(PathBuf::from("../../target").join(std::env::var("PROFILE").unwrap())),
     )
