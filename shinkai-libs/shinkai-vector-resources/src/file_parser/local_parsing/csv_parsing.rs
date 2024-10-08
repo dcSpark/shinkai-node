a1da5ba5acb83cb232f3689f45ca8fb6ae22feec
benolt <bence.oltean@gmail.com>
2024-10-08 11:04:37 +0200

    Join CSV table into text group

--- shinkai-libs/shinkai-vector-resources/src/file_parser/local_parsing/csv_parsing.rs
+++ shinkai-libs/shinkai-vector-resources/src/file_parser/local_parsing/csv_parsing.rs
@@ -11,8 +11,33 @@ impl LocalFileParser {
     pub fn process_csv_file(file_buffer: Vec<u8>, max_node_text_size: u64) -> Result<Vec<TextGroup>, VRError> {
         let csv_lines = Self::parse_csv_auto(&file_buffer).map_err(|_| VRError::FailedCSVParsing)?;
 
-        let mut text_groups = Vec::new();
+        // Join as many lines as possible into a text group.
+        let mut csv_lines_split = Vec::new();
+        let mut current_group = Vec::new();
+        let mut current_length = 0;
+
         for line in csv_lines {
+            let line_length = line.len() as u64;
+            if current_length + line_length > max_node_text_size {
+                csv_lines_split.push(current_group);
+                current_group = Vec::new();
+                current_length = 0;
+            }
+            current_group.push(line);
+            current_length += line_length;
+        }
+
+        if !current_group.is_empty() {
+            csv_lines_split.push(current_group);
+        }
+
+        let joined_lines = csv_lines_split
+            .into_iter()
+            .map(|group| group.join("\n"))
+            .collect::<Vec<String>>();
+
+        let mut text_groups = Vec::new();
+        for line in joined_lines {
             let (parsed_line, metadata, parsed_any_metadata) = ShinkaiFileParser::parse_and_extract_metadata(&line);
 
             if parsed_line.len() as u64 > max_node_text_size {
@@ -67,7 +92,7 @@ impl LocalFileParser {
             .collect::<Vec<String>>();
 
         let likely_header = headers.iter().all(|s| {
-            let is_alphabetic = s.chars().all(|c| c.is_alphabetic() || c.is_whitespace());
+            let is_alphabetic = s.chars().all(|c| c.is_alphabetic() || c.is_whitespace() || c == '_');
             let no_duplicates = headers.iter().filter(|&item| item == s).count() == 1;
             let no_prohibited_chars = !s.contains(&['@', '#', '$', '%', '^', '&', '*']);
 
@@ -81,7 +106,10 @@ impl LocalFileParser {
     // /// * `header` - A boolean indicating whether to prepend column headers to
     // ///   values.
     pub fn parse_csv(buffer: &[u8], header: bool) -> Result<Vec<String>, VRError> {
-        let mut reader = ReaderBuilder::new().flexible(true).from_reader(Cursor::new(buffer));
+        let mut reader = ReaderBuilder::new()
+            .flexible(true)
+            .has_headers(header)
+            .from_reader(Cursor::new(buffer));
         let headers = if header {
             reader
                 .headers()
@@ -105,7 +133,7 @@ impl LocalFileParser {
             } else {
                 record.iter().map(String::from).collect()
             };
-            let row_string = row.join(", ");
+            let row_string = row.join("|");
             result.push(row_string);
         }
 
