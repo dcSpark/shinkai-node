d31f14a7252a6d3fae92d7eec3573417726854bc
Edward Alvarado <ac.edward@gmail.com>
2024-11-13 20:03:49 -0300

    Update Prompts

--- shinkai-bin/shinkai-node/src/tools/tool_generation.rs
+++ shinkai-bin/shinkai-node/src/tools/tool_generation.rs
@@ -158,7 +158,7 @@ pub async fn tool_implementation(
             match language {
                 Language::Typescript => {
                     generate_code_prompt.push_str(&format!(
-                        "
+                        r####"
 # RULE I:
 * You may use any of the following functions if they are relevant and a good match for the task.
 * Import them in the following way (do not rename functions with 'as'):
@@ -194,7 +194,7 @@ export async function run(config: CONFIG, inputs: INPUTS): Promise<OUTPUT> {{
 
 # INPUT:
 {prompt_text}
-"
+"####
                     ));
                 }
                 Language::Python => {
@@ -259,80 +259,225 @@ pub async fn tool_metadata_implementation(
     match language {
         Language::Typescript => {
             generate_code_prompt.push_str(&format!(
-                "
-# RULE I: 
+                r####"
+# RULE I:
+This is the SCHEMA for the METADATA:
+```json
+META_SCHEMA = {{
+  "name": "metaschema",
+  "schema": {{
+    "type": "object",
+    "properties": {{
+      "name": {{
+        "type": "string",
+        "description": "The name of the schema"
+      }},
+      "type": {{
+        "type": "string",
+        "enum": [
+          "object",
+          "array",
+          "string",
+          "number",
+          "boolean",
+          "null"
+        ]
+      }},
+      "properties": {{
+        "type": "object",
+        "additionalProperties": {{
+          "$ref": "#/$defs/schema_definition"
+        }}
+      }},
+      "items": {{
+        "anyOf": [
+          {{
+            "$ref": "#/$defs/schema_definition"
+          }},
+          {{
+            "type": "array",
+            "items": {{
+              "$ref": "#/$defs/schema_definition"
+            }}
+          }}
+        ]
+      }},
+      "required": {{
+        "type": "array",
+        "items": {{
+          "type": "string"
+        }}
+      }},
+      "additionalProperties": {{
+        "type": "boolean"
+      }}
+    }},
+    "required": [
+      "type"
+    ],
+    "additionalProperties": false,
+    "if": {{
+      "properties": {{
+        "type": {{
+          "const": "object"
+        }}
+      }}
+    }},
+    "then": {{
+      "required": [
+        "properties"
+      ]
+    }},
+    "$defs": {{
+      "schema_definition": {{
+        "type": "object",
+        "properties": {{
+          "type": {{
+            "type": "string",
+            "enum": [
+              "object",
+              "array",
+              "string",
+              "number",
+              "boolean",
+              "null"
+            ]
+          }},
+          "properties": {{
+            "type": "object",
+            "additionalProperties": {{
+              "$ref": "#/$defs/schema_definition"
+            }}
+          }},
+          "items": {{
+            "anyOf": [
+              {{
+                "$ref": "#/$defs/schema_definition"
+              }},
+              {{
+                "type": "array",
+                "items": {{
+                  "$ref": "#/$defs/schema_definition"
+                }}
+              }}
+            ]
+          }},
+          "required": {{
+            "type": "array",
+            "items": {{
+              "type": "string"
+            }}
+          }},
+          "additionalProperties": {{
+            "type": "boolean"
+          }}
+        }},
+        "required": [
+          "type"
+        ],
+        "additionalProperties": false,
+        "if": {{
+          "properties": {{
+            "type": {{
+              "const": "object"
+            }}
+          }}
+        }},
+        "then": {{
+          "required": [
+            "properties"
+          ]
+        }}
+      }}
+    }}
+  }}
+}}
+```
+
 These are two examples of METADATA:
 ## Example 1:
 ```json
 {{
-  id: 'shinkai-tool-coinbase-create-wallet',
-  name: 'Shinkai: Coinbase Wallet Creator',
-  description: 'Tool for creating a Coinbase wallet',
-  author: 'Shinkai',
-  keywords: ['coinbase', 'wallet', 'creator', 'shinkai'],
-  configurations: {{
-    type: 'object',
-    properties: {{
-      name: {{ type: 'string' }},
-      privateKey: {{ type: 'string' }},
-      useServerSigner: {{ type: 'string', default: 'false', nullable: true }},
+  "id": "shinkai-tool-coinbase-create-wallet",
+  "name": "Shinkai: Coinbase Wallet Creator",
+  "description": "Tool for creating a Coinbase wallet",
+  "author": "Shinkai",
+  "keywords": [
+    "coinbase",
+    "wallet",
+    "creator",
+    "shinkai"
+  ],
+  "configurations": {{
+    "type": "object",
+    "properties": {{
+      "name": {{ "type": "string" }},
+      "privateKey": {{ "type": "string" }},
+      "useServerSigner": {{ "type": "string", "default": "false", "nullable": true }},
     }},
-    required: [{{'name', 'privateKey'}}],
+    "required": [
+      "name",
+      "privateKey"
+    ]
   }},
-  parameters: {{
-    type: 'object',
-    properties: {{}},
-    required: [], // No required parameters
+  "parameters": {{
+    "type": "object",
+    "properties": {{}},
+    "required": [],
   }},
-  result: {{
-    type: 'object',
-    properties: {{
-      walletId: {{ type: 'string', nullable: true }},
-      seed: {{ type: 'string', nullable: true }},
-      address: {{ type: 'string', nullable: true }},
+  "result": {{
+    "type": "object",
+    "properties": {{
+      "walletId": {{ "type": "string", "nullable": true }},
+      "seed": {{ "type": "string", "nullable": true }},
+      "address": {{ "type": "string", "nullable": true }},
     }},
-    required: [],
-  }},
+    "required": [],
+  }}
 }};
 ```
 
 ## Example 2:
 ```json
 {{
-  id: 'shinkai-tool-download-pages',
-  name: 'Shinkai: Download Pages',
-  description: 'Downloads one or more URLs and converts their HTML content to Markdown',
-  author: 'Shinkai',
-  keywords: [
-    'HTML to Markdown',
-    'web page downloader',
-    'content conversion',
-    'URL to Markdown',
+  "id": "shinkai-tool-download-pages",
+  "name": "Shinkai: Download Pages",
+  "description": "Downloads one or more URLs and converts their HTML content to Markdown",
+  "author": "Shinkai",
+  "keywords": [
+    "HTML to Markdown",
+    "web page downloader",
+    "content conversion",
+    "URL to Markdown",
   ],
-  configurations: {{
-    type: 'object',
-    properties: {{}},
-    required: [],
+  "configurations": {{
+    "type": "object",
+    "properties": {{}},
+    "required": [],
   }},
-  parameters: {{
-    type: 'object',
-    properties: {{
-      urls: {{ type: 'array', items: {{ type: 'string' }} }},
+  "parameters": {{
+    "type": "object",
+    "properties": {{
+      "urls": {{ "type": "array", "items": {{ "type": "string" }} }},
     }},
-    required: [{{'urls'}}],
+    "required": [
+      "urls"
+    ]
   }},
-  result: {{
-    type: 'object',
-    properties: {{
-      markdowns: {{ type: 'array', items: {{ type: 'string' }} }},
+  "result": {{
+    "type": "object",
+    "properties": {{
+      "markdowns": {{ "type": "array", "items": {{ "type": "string" }} }},
     }},
-    required: [{{'markdowns'}}],
-  }},
+    "required": [
+      "markdowns"
+    ]
+  }}
 }};
 ```
 
 # RULE II:
-* Following the format of the examples provided.
+* Return a valid schema for the described JSON.
 * The METADATA must be in JSON format.
 * Output only the METADATA, so the complete Output it's a valid JSON string.
 * Any comments, notes, explanations or examples must be omitted in the Output.
@@ -342,7 +487,7 @@ These are two examples of METADATA:
 ```{language}
 {}
 ```
-",
+"####,
                 code.unwrap_or("".to_string())
             ));
         }
