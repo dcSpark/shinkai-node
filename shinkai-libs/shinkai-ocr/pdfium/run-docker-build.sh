55d69584048b938d7d5cc16054c37c76043677f6
Guillermo Valin <contacto@guillermovalin.com>
2024-11-27 21:57:48 -0300

    Enhance Dockerfile and build scripts for improved PDFium build configuration
    - Added support for dynamic linking and ARM64 architecture in build scripts
    - Updated Dockerfile to include additional dependencies
    - Modified build.sh to handle linking type and CPU architecture dynamically

--- shinkai-libs/shinkai-ocr/pdfium/run-docker-build.sh
+++ shinkai-libs/shinkai-ocr/pdfium/run-docker-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -eu
 
 TIMESTAMP=$(date +%y%m%d%H%M%S)
+CPU=arm64
+LINKING=dynamic
 
-docker build -t build-pdfium-$TIMESTAMP -f Dockerfile .
+docker build --platform linux/amd64 -t build-pdfium-$TIMESTAMP -f Dockerfile .
 
-docker run -v $(PWD)/linux-x64:/app/linux-x64 --name build-pdfium-$TIMESTAMP build-pdfium-$TIMESTAMP
+docker run --platform linux/amd64 -v $(PWD)/linux-$CPU:/app/linux-$CPU -e LINKING=$LINKING -e CPU=$CPU --name build-pdfium-$TIMESTAMP build-pdfium-$TIMESTAMP
