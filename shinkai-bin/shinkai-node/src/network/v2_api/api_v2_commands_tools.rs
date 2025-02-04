12ef4fcfd0c5e0e0abe627d3a59a5f191e076572
Nico Arqueros <nico@dcspark.io>
2025-02-03 19:03:01 -0600

    update store url

--- shinkai-bin/shinkai-node/src/network/v2_api/api_v2_commands_tools.rs
+++ shinkai-bin/shinkai-node/src/network/v2_api/api_v2_commands_tools.rs
@@ -1,14 +1,7 @@
 use crate::{
-    llm_provider::job_manager::JobManager,
-    managers::IdentityManager,
-    network::{node_error::NodeError, node_shareable_logic::download_zip_file, Node},
-    tools::{
-        tool_definitions::definition_generation::{generate_tool_definitions, get_all_deno_tools},
-        tool_execution::execution_coordinator::{execute_code, execute_tool_cmd},
-        tool_generation::v2_create_and_send_job_message,
-        tool_prompts::{generate_code_prompt, tool_metadata_implementation_prompt},
-    },
-    utils::environment::NodeEnvironment,
+    llm_provider::job_manager::JobManager, managers::IdentityManager, network::{node_error::NodeError, node_shareable_logic::download_zip_file, Node}, tools::{
+        tool_definitions::definition_generation::{generate_tool_definitions, get_all_deno_tools}, tool_execution::execution_coordinator::{execute_code, execute_tool_cmd}, tool_generation::v2_create_and_send_job_message, tool_prompts::{generate_code_prompt, tool_metadata_implementation_prompt}
+    }, utils::environment::NodeEnvironment
 };
 
 use async_channel::Sender;
@@ -19,36 +12,21 @@ use serde_json::{json, Map, Value};
 use shinkai_http_api::node_api_router::{APIError, SendResponseBodyData};
 use shinkai_message_primitives::{
     schemas::{
-        inbox_name::InboxName, indexable_version::IndexableVersion, job::JobLike, job_config::JobConfig,
-        shinkai_name::ShinkaiSubidentityType, tool_router_key::ToolRouterKey,
-    },
-    shinkai_message::shinkai_message_schemas::{CallbackAction, JobCreationInfo, MessageSchemaType},
-    shinkai_utils::{shinkai_message_builder::ShinkaiMessageBuilder, signatures::clone_signature_secret_key},
+        inbox_name::InboxName, indexable_version::IndexableVersion, job::JobLike, job_config::JobConfig, shinkai_name::ShinkaiSubidentityType, tool_router_key::ToolRouterKey
+    }, shinkai_message::shinkai_message_schemas::{CallbackAction, JobCreationInfo, MessageSchemaType}, shinkai_utils::{shinkai_message_builder::ShinkaiMessageBuilder, signatures::clone_signature_secret_key}
 };
 use shinkai_message_primitives::{
     schemas::{
-        shinkai_name::ShinkaiName,
-        shinkai_tools::{CodeLanguage, DynamicToolType},
-    },
-    shinkai_message::shinkai_message_schemas::JobMessage,
+        shinkai_name::ShinkaiName, shinkai_tools::{CodeLanguage, DynamicToolType}
+    }, shinkai_message::shinkai_message_schemas::JobMessage
 };
 use shinkai_sqlite::{errors::SqliteManagerError, SqliteManager};
 use shinkai_tools_primitives::tools::{
-    deno_tools::DenoTool,
-    error::ToolError,
-    python_tools::PythonTool,
-    shinkai_tool::{ShinkaiTool, ShinkaiToolWithAssets},
-    tool_config::{OAuth, ToolConfig},
-    tool_output_arg::ToolOutputArg,
-    tool_playground::ToolPlayground,
+    deno_tools::DenoTool, error::ToolError, python_tools::PythonTool, shinkai_tool::{ShinkaiTool, ShinkaiToolWithAssets}, tool_config::{OAuth, ToolConfig}, tool_output_arg::ToolOutputArg, tool_playground::ToolPlayground
 };
 
 use std::{
-    env,
-    fs::File,
-    io::{Read, Write},
-    sync::Arc,
-    time::Instant,
+    env, fs::File, io::{Read, Write}, sync::Arc, time::Instant
 };
 use tokio::sync::Mutex;
 use zip::{write::FileOptions, ZipWriter};
@@ -1748,8 +1726,8 @@ impl Node {
         println!("[Publish Tool] Signature: {}", signature_hex.clone());
         println!("[Publish Tool] Identity: {}", identity_name.clone());
 
-        let store_url = env::var("SHINKAI_STORE_URL")
-            .unwrap_or("https://shinkai-store-302883622007.us-central1.run.app".to_string());
+        let store_url =
+            env::var("SHINKAI_STORE_URL").unwrap_or("https://store-api.shinkai.com/store/defaults".to_string());
         let response = client
             .post(format!("{}/store/revisions", store_url))
             .multipart(form)
@@ -1822,12 +1800,13 @@ impl Node {
         node_name: String,
         signing_secret_key: SigningKey,
     ) -> Result<Value, APIError> {
-        let mut zip_contents = match download_zip_file(url, "__tool.json".to_string(), node_name, signing_secret_key).await {
-            Ok(contents) => contents,
-            Err(err) => {
-                return Err(err);
-            }
-        };
+        let mut zip_contents =
+            match download_zip_file(url, "__tool.json".to_string(), node_name, signing_secret_key).await {
+                Ok(contents) => contents,
+                Err(err) => {
+                    return Err(err);
+                }
+            };
 
         // Parse the JSON into a ShinkaiTool
         let tool: ShinkaiTool = match serde_json::from_slice(&zip_contents.buffer) {
@@ -2446,8 +2425,8 @@ impl Node {
             return Ok(());
         }
 
-        let store_url = env::var("SHINKAI_STORE_URL")
-            .unwrap_or("https://shinkai-store-302883622007.us-central1.run.app".to_string());
+        let store_url =
+            env::var("SHINKAI_STORE_URL").unwrap_or("https://store-api.shinkai.com/store/defaults".to_string());
 
         let client = reqwest::Client::new();
 
