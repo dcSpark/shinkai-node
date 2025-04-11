c360127160fa143c6dbd2c1e96679b91a4d5ed2e
Alfredo Gallardo <agallardol@outlook.com>
2025-04-11 17:24:54 -0400

    feat: add database_name parameter to sql processor

--- shinkai-bin/shinkai-node/src/tools/tool_implementation/native_tools/sql_processor.rs
+++ shinkai-bin/shinkai-node/src/tools/tool_implementation/native_tools/sql_processor.rs
@@ -71,6 +71,7 @@ SELECT field_1, field_3 FROM table_name WHERE field_3 > 100 ORDER BY field_2 DES
                     let mut params = Parameters::new();
                     params.add_property("query".to_string(), "string".to_string(), "The SQL query to execute".to_string(), true);
                     params.add_property("params".to_string(), "any[]".to_string(), "The parameters to pass to the query".to_string(), false);
+                    params.add_property("database_name".to_string(), "string".to_string(), "By default, the database name is the app_id. You can specify a different name to share the same database in multiple contexts.".to_string(), false);
                     params
                 },
                 output_arg: ToolOutputArg {
@@ -96,6 +97,19 @@ fn get_folder_path(app_id: String) -> Result<PathBuf, ToolError> {
         .join(app_id)
         .join("home")
         .join("db.sqlite"))
+}
+
+fn get_database_path_from_db_name_config(database_name: String) -> Result<PathBuf, ToolError> {
+    let node_env = fetch_node_environment();
+    let node_storage_path = node_env
+        .node_storage_path
+        .clone()
+        .ok_or_else(|| ToolError::ExecutionError("Node storage path is not set".to_string()))?;
+    let adapted_database_name = database_name.chars().map(|c| if c.is_alphanumeric() { c } else { '-' }).collect::<String>();
+    Ok(Path::new(&node_storage_path)
+        .join("tools_storage")
+        .join("shared_sql_databases")
+        .join(format!("{}.sqlite", adapted_database_name)))
 }
 
 pub async fn get_current_tables(app_id: String) -> Result<Vec<String>, ToolError> {
@@ -158,7 +172,17 @@ impl ToolExecutor for SQLProcessorTool {
                     .collect::<Vec<&str>>()
             })
             .unwrap_or(vec![]);
-        let full_path = get_folder_path(app_id)?;
+
+        let database_name = parameters
+            .get("database_name")
+            .and_then(|v| v.as_str());
+
+        let full_path = if let Some(database_name) = database_name {
+            get_database_path_from_db_name_config(database_name.to_string())?
+        } else {
+            get_folder_path(app_id)?
+        };
+
         // Ensure parent directory exists
         if let Some(parent) = full_path.parent() {
             std::fs::create_dir_all(parent)
