1884ded8024eed552a6c3169adfeed09ba49e865
Alfredo Gallardo <agallardol@outlook.com>
2025-05-07 18:01:57 -0400

    feat: added parse docx to remove docx-rs dependency

--- shinkai-libs/shinkai-fs/src/simple_parser/local_parsing/pdf_parsing.rs
+++ shinkai-libs/shinkai-fs/src/simple_parser/local_parsing/pdf_parsing.rs
@@ -1,9 +1,6 @@
-use std::{collections::HashMap, env, path::PathBuf};
+use std::path::PathBuf;
 
-use serde_json::json;
-use shinkai_tools_runner::tools::{
-    code_files::CodeFiles, deno_runner::DenoRunner, deno_runner_options::DenoRunnerOptions, execution_context::ExecutionContext, python_runner::PythonRunner, python_runner_options::PythonRunnerOptions, runner_type::RunnerType
-};
+use shinkai_non_rust_code::functions::parse_pdf::parse_pdf;
 
 use crate::{
     shinkai_fs_error::ShinkaiFsError, simple_parser::{file_parser_helper::ShinkaiFileParser, text_group::TextGroup}
@@ -11,101 +8,12 @@ use crate::{
 
 use super::LocalFileParser;
 
-#[derive(serde::Deserialize)]
-pub struct PDFParseResult {
-    pub pages: Vec<Page>,
-}
-
-#[derive(serde::Deserialize)]
-pub struct Page {
-    pub metadata: Metadata,
-    pub text: String,
-}
-
-#[derive(serde::Deserialize)]
-pub struct Metadata {
-    pub page: u32,
-}
-
-pub async fn parse_pdf_file(file_path: PathBuf) -> Result<PDFParseResult, ShinkaiFsError> {
-    println!("parsing PDF file: {:?}", file_path);
-    let code_files = CodeFiles {
-        files: HashMap::from([(
-            "main.py".to_string(),
-            r#"
-# /// script
-# requires-python = ">=3.10,<3.13"
-# dependencies = [
-#   "pymupdf4llm",
-# ]
-# ///
-import pymupdf4llm
-
-class CONFIG:
-    pass
-
-class INPUTS:
-    file_path: str
-
-class OUTPUT:
-    pages: object
-
-async def run(c: CONFIG, p: INPUTS) -> OUTPUT:
-    parsed_pages = pymupdf4llm.to_markdown(p.file_path, page_chunks=True)
-    output = OUTPUT()
-    output.pages = parsed_pages
-    return output
-"#
-            .to_string(),
-        )]),
-        entrypoint: "main.py".to_string(),
-    };
-    let deno_runner = PythonRunner::new(
-        code_files,
-        json!({}),
-        Some(PythonRunnerOptions {
-            uv_binary_path: PathBuf::from(
-                env::var("SHINKAI_TOOLS_RUNNER_UV_BINARY_PATH")
-                    .unwrap_or_else(|_| "./shinkai-tools-runner-resources/uv".to_string()),
-            ),
-            context: ExecutionContext {
-                storage: PathBuf::from(env::var("NODE_STORAGE_PATH").unwrap_or_else(|_| "./".to_string()))
-                    .join("internal_tools_storage"),
-
-                context_id: "shinkai-node-pdf-parsing".to_string(),
-                mount_files: vec![file_path.clone()],
-                ..Default::default()
-            },
-            force_runner_type: Some(RunnerType::Host),
-            ..Default::default()
-        }),
-    );
-
-    let start = std::time::Instant::now();
-    let result = deno_runner
-        .run(
-            None,
-            json!({
-                "file_path": file_path.to_str().unwrap(),
-            }),
-            None,
-        )
-        .await
-        .inspect_err(|e| {
-            println!("Error: {:?}", e);
-        })
-        .map_err(|_| ShinkaiFsError::FailedPDFParsing)?;
-    println!("PDF parsing took: {:?}", start.elapsed());
-    let data = serde_json::from_value::<PDFParseResult>(result.data).map_err(|_| ShinkaiFsError::FailedPDFParsing)?;
-    Ok(data)
-}
-
 impl LocalFileParser {
     pub async fn process_pdf_file(
         file_path: PathBuf,
         max_node_text_size: u64,
     ) -> Result<Vec<TextGroup>, ShinkaiFsError> {
-        let parsed_pages = parse_pdf_file(file_path)
+        let parsed_pages = parse_pdf(file_path)
             .await
             .map_err(|_| ShinkaiFsError::FailedPDFParsing)?;
 
@@ -122,30 +30,5 @@ impl LocalFileParser {
         }
 
         Ok(text_groups)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::test_utils::testing_create_tempdir_and_set_env_var;
-
-    use super::*;
-    use std::path;
-    use std::path::Path;
-
-    #[tokio::test]
-    async fn test_parse_pdf_file() {
-        let _dir = testing_create_tempdir_and_set_env_var();
-
-        let file_path = path::absolute(Path::new("../../files/Shinkai_Protocol_Whitepaper.pdf"))
-            .unwrap()
-            .to_path_buf();
-        let result = parse_pdf_file(file_path).await;
-        assert!(result.is_ok());
-        let parsed_pdf = result.unwrap();
-        assert!(parsed_pdf.pages.len() == 14);
-        assert!(parsed_pdf.pages[13]
-            .text
-            .contains("Essential for MAC\ncomputation, defined as"));
     }
 }
--- shinkai-libs/shinkai-fs/src/simple_parser/local_parsing/xlsx_parsing.rs
+++ shinkai-libs/shinkai-fs/src/simple_parser/local_parsing/xlsx_parsing.rs
@@ -1,103 +1,37 @@
-use serde_json::{json, Value};
-use shinkai_tools_runner::tools::{
-    code_files::CodeFiles, deno_runner::DenoRunner, deno_runner_options::DenoRunnerOptions, execution_context::ExecutionContext, runner_type::RunnerType
-};
+use serde_json::Value;
+use shinkai_non_rust_code::functions::parse_xlsx::parse_xlsx;
 
 use crate::{shinkai_fs_error::ShinkaiFsError, simple_parser::text_group::TextGroup};
 
-use std::{
-    collections::HashMap, env, path::{Path, PathBuf}
-};
+use std::path::PathBuf;
 
 use super::LocalFileParser;
 
-pub async fn parse_xlsx_to_matrix(xlsx_file_path: PathBuf) -> Result<Vec<Vec<String>>, ShinkaiFsError> {
-    println!("parsing XLSX file: {:?}", xlsx_file_path);
-    let code_files = CodeFiles {
-        files: HashMap::from([(
-            "main.ts".to_string(),
-            r#"
-            // @deno-types="https://cdn.sheetjs.com/xlsx-0.20.3/package/types/index.d.ts"
-            import * as XLSX from 'https://cdn.sheetjs.com/xlsx-0.20.3/package/xlsx.mjs';
-
-            async function run(configurations, params) {
-                console.log(params.file);
-                const workbook = XLSX.read(params.file, {type: 'file'});
-                const firstSheetName = workbook.SheetNames[0];
-                const worksheet = workbook.Sheets[firstSheetName];
-                const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1, defval: null});
-                console.log("Sheet name: ", firstSheetName);
-                return {
-                    rows
-                };
-            }
-            "#
-            .to_string(),
-        )]),
-        entrypoint: "main.ts".to_string(),
-    };
-    let deno_runner = DenoRunner::new(
-        code_files,
-        json!({}),
-        Some(DenoRunnerOptions {
-            deno_binary_path: PathBuf::from(
-                env::var("SHINKAI_TOOLS_RUNNER_DENO_BINARY_PATH")
-                    .unwrap_or_else(|_| "./shinkai-tools-runner-resources/deno".to_string()),
-            ),
-            context: ExecutionContext {
-                storage: PathBuf::from(env::var("NODE_STORAGE_PATH").unwrap_or_else(|_| "./".to_string()))
-                    .join("internal_tools_storage"),
-                context_id: "shinkai-node-xlsx-parsing".to_string(),
-                mount_files: vec![xlsx_file_path.clone()],
-                ..Default::default()
-            },
-            force_runner_type: Some(RunnerType::Host),
-            ..Default::default()
-        }),
-    );
-
-    let result = deno_runner
-        .run(
-            None,
-            json!({
-                "file": xlsx_file_path.to_str().unwrap(),
-            }),
-            None,
-        )
-        .await
-        .inspect_err(|e| {
-            println!("Error: {:?}", e);
-        })
-        .map_err(|_| ShinkaiFsError::FailedXLSXParsing)?;
-
-    let rows: Vec<Vec<String>> = result.data["rows"]
-        .as_array()
-        .ok_or(ShinkaiFsError::FailedXLSXParsing)?
-        .iter()
-        .map(|row| -> Result<Vec<String>, ShinkaiFsError> {
-            Ok(row
-                .as_array()
-                .ok_or(ShinkaiFsError::FailedXLSXParsing)?
-                .iter()
-                .map(|cell| match cell {
-                    Value::String(s) => s.to_string(),
-                    Value::Number(n) => n.to_string(),
-                    Value::Bool(b) => b.to_string(),
-                    _ => "".to_string(),
-                })
-                .collect())
-        })
-        .collect::<Result<Vec<Vec<String>>, ShinkaiFsError>>()?;
-
-    Ok(rows)
-}
-
 impl LocalFileParser {
     pub async fn parse_xlsx(file_path: PathBuf) -> Result<Vec<String>, ShinkaiFsError> {
-        let xlsx_lines = parse_xlsx_to_matrix(file_path)
+        let parsed_xlsx = parse_xlsx(file_path)
             .await
             .map_err(|_| ShinkaiFsError::FailedXLSXParsing)?;
-        let parsed_lines = xlsx_lines.into_iter().map(|row| row.join("|")).collect::<Vec<String>>();
+
+        let parsed_xlsx: Vec<Vec<String>> = parsed_xlsx
+            .rows
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .map(|cell| match cell {
+                        Value::String(s) => s.to_string(),
+                        Value::Number(n) => n.to_string(),
+                        Value::Bool(b) => b.to_string(),
+                        _ => "".to_string(),
+                    })
+                    .collect::<Vec<String>>()
+            })
+            .collect();
+
+        let parsed_lines = parsed_xlsx
+            .into_iter()
+            .map(|row| row.join("|"))
+            .collect::<Vec<String>>();
         Ok(parsed_lines)
     }
 
@@ -105,11 +39,26 @@ impl LocalFileParser {
         file_path: PathBuf,
         max_node_text_size: u64,
     ) -> Result<Vec<TextGroup>, ShinkaiFsError> {
-        let xlsx_lines = Self::parse_xlsx(file_path)
+        let parsed_xls = parse_xlsx(file_path)
             .await
             .map_err(|_| ShinkaiFsError::FailedXLSXParsing)?;
-        println!("xlsx_lines: {:?}", xlsx_lines[0]);
-        Self::process_table_rows(xlsx_lines, max_node_text_size)
+        let parsed_xls: Vec<Vec<String>> = parsed_xls
+            .rows
+            .iter()
+            .map(|row| -> Result<Vec<String>, ShinkaiFsError> {
+                Ok(row
+                    .iter()
+                    .map(|cell| match cell {
+                        Value::String(s) => s.to_string(),
+                        Value::Number(n) => n.to_string(),
+                        Value::Bool(b) => b.to_string(),
+                        _ => "".to_string(),
+                    })
+                    .collect())
+            })
+            .collect::<Result<Vec<Vec<String>>, ShinkaiFsError>>()?;
+        let parsed_lines = parsed_xls.into_iter().map(|row| row.join("|")).collect::<Vec<String>>();
+        Self::process_table_rows(parsed_lines, max_node_text_size)
     }
 }
 
@@ -119,28 +68,6 @@ mod tests {
     use crate::test_utils::testing_create_tempdir_and_set_env_var;
     use std::path;
     use std::path::Path;
-
-    #[tokio::test]
-    async fn test_parse_xlsx_to_matrix() {
-        let _dir = testing_create_tempdir_and_set_env_var();
-
-        let xlsx_file_path = path::absolute(Path::new("./src/test_data/test.xlsx"))
-            .unwrap()
-            .to_path_buf();
-        let rows = parse_xlsx_to_matrix(xlsx_file_path).await.unwrap();
-        assert_eq!(rows.len(), 4);
-        assert_eq!(rows[0].len(), 2);
-    }
-
-    #[tokio::test]
-    async fn test_parse_xls_to_matrix() {
-        let xlsx_file_path = path::absolute(Path::new("./src/test_data/test.xls"))
-            .unwrap()
-            .to_path_buf();
-        let rows = parse_xlsx_to_matrix(xlsx_file_path).await.unwrap();
-        assert_eq!(rows.len(), 11);
-        assert_eq!(rows[0].len(), 8);
-    }
 
     #[tokio::test]
     async fn test_process_xlsx_file() {
