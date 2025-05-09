fd07de0648323de5b39e5dac8b3f608050d0715a
Nico Arqueros <nico@dcspark.io>
2025-05-09 13:11:02 -0500

    fix

--- shinkai-bin/shinkai-node/src/managers/model_capabilities_manager.rs
+++ shinkai-bin/shinkai-node/src/managers/model_capabilities_manager.rs
@@ -384,9 +384,10 @@ impl ModelCapabilitiesManager {
     pub fn get_max_tokens(model: &LLMProviderInterface) -> usize {
         match model {
             LLMProviderInterface::OpenAI(openai) => {
-                if openai.model_type.starts_with("gpt-4o")
+                if openai.model_type.contains("4o-mini") {
+                    16_000
+                } else if openai.model_type.starts_with("gpt-4o")
                     || openai.model_type.starts_with("gpt-4-1106-preview")
-                    || openai.model_type.starts_with("gpt-4o-mini")
                     || openai.model_type.starts_with("gpt-4-vision-preview")
                     || openai.model_type.starts_with("o1-mini")
                     || openai.model_type.starts_with("o1-preview")
@@ -529,7 +530,9 @@ impl ModelCapabilitiesManager {
     pub fn get_max_output_tokens(model: &LLMProviderInterface) -> usize {
         match model {
             LLMProviderInterface::OpenAI(openai) => {
-                if openai.model_type.starts_with("o1-preview")
+                if openai.model_type.contains("4o-mini") {
+                    16_000
+                } else if openai.model_type.starts_with("o1-preview")
                     || openai.model_type.starts_with("o1-mini")
                     || openai.model_type.starts_with("gpt-4.1")
                 {
