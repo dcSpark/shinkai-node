3bc28064fd786d37161c9a2fbbe9e71520ec6277
Guillermo Valin <contacto@guillermovalin.com>
2025-05-12 19:34:34 -0400

    Preload tools on tests.

--- .github/run-main-cargo-tests.sh
+++ .github/run-main-cargo-tests.sh
@@ -2,4 +2,5 @@
 
 export IS_TESTING=1
 export WELCOME_MESSAGE=false
+export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
 cd /app && cargo test -- --test-threads=1
--- .github/run-main-primitives-cargo-tests.sh
+++ .github/run-main-primitives-cargo-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
 cd /app/shinkai-libs/shinkai-message-primitives && cargo test -- --test-threads=1
 
