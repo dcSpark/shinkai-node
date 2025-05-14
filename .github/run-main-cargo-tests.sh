a3b18106a81b858b39e73d3532d87826b71e1411
Guillermo Valin <contacto@guillermovalin.com>
2025-05-14 14:15:24 -0400

    Deactivate pre-install from folder.

--- .github/run-main-cargo-tests.sh
+++ .github/run-main-cargo-tests.sh
@@ -3,5 +3,5 @@
 export IS_TESTING=1
 export SKIP_IMPORT_FROM_DIRECTORY=true
 export WELCOME_MESSAGE=false
-export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
-cd /app && cargo test -- --test-threads=1
+# export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
+cd /app && cargo test -- --test-threads=1 --nocapture
