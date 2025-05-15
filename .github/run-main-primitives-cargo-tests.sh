9822f1f08dc0818c9ba279579b84aa5b986eff80
Guillermo Valin <contacto@guillermovalin.com>
2025-05-15 09:58:43 -0400

    Omit INSTALL_FOLDER_PATH.

--- .github/run-main-primitives-cargo-tests.sh
+++ .github/run-main-primitives-cargo-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
+# export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
 cd /app/shinkai-libs/shinkai-message-primitives && cargo test -- --test-threads=1 --nocapture
 
