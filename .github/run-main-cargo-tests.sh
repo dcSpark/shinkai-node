8650cfaea600aa25ec3840cc1b3d5b12e6652a6c
Guillermo Valin <contacto@guillermovalin.com>
2025-05-16 09:08:49 -0400

    Fix docker image

--- .github/run-main-cargo-tests.sh
+++ .github/run-main-cargo-tests.sh
@@ -4,4 +4,4 @@ export IS_TESTING=1
 export SKIP_IMPORT_FROM_DIRECTORY=true
 export WELCOME_MESSAGE=false
 # export INSTALL_FOLDER_PATH=${INSTALL_FOLDER_PATH:-"/app/pre-install"}
-cd /app/shinkai-node && cargo test -- --test-threads=1 --nocapture
+cd /app && cargo test -- --test-threads=1 --nocapture
