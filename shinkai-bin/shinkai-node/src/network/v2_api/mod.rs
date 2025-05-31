4d7c5d389b62dc18741e2ddbef8c55ac3297735d
Nico Arqueros <nico@dcspark.io>
2025-05-31 16:11:39 -0500

    fix

--- shinkai-bin/shinkai-node/src/network/v2_api/mod.rs
+++ shinkai-bin/shinkai-node/src/network/v2_api/mod.rs
@@ -3,12 +3,17 @@ pub mod api_v2_commands_cron;
 pub mod api_v2_commands_ext_agent_offers;
 pub mod api_v2_commands_jobs;
 pub mod api_v2_commands_my_agent_offers;
-pub mod api_v2_commands_oauth;
 #[cfg(feature = "ngrok")]
 pub mod api_v2_commands_ngrok;
 #[cfg(not(feature = "ngrok"))]
-pub mod api_v2_commands_ngrok_disabled as api_v2_commands_ngrok;
+pub mod api_v2_commands_ngrok_disabled;
+pub mod api_v2_commands_oauth;
 pub mod api_v2_commands_prompts;
 pub mod api_v2_commands_tools;
 pub mod api_v2_commands_vecfs;
 pub mod api_v2_commands_wallets;
+
+#[cfg(feature = "ngrok")]
+pub use api_v2_commands_ngrok;
+#[cfg(not(feature = "ngrok"))]
+pub use api_v2_commands_ngrok_disabled as api_v2_commands_ngrok;
