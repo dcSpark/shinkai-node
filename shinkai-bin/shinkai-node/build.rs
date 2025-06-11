8bb285b7d842c08dae144987622de3414ef8104a
Alfredo Gallardo <agallardol@outlook.com>
2025-06-11 10:48:47 -0400

    chore: removed re-run if change on copy code runner assets

--- shinkai-bin/shinkai-node/build.rs
+++ shinkai-bin/shinkai-node/build.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=shinkai-tools-runner-resources");
     shinkai_tools_runner::copy_assets::copy_assets(
         Some(PathBuf::from("./")),
         Some(PathBuf::from("../../target").join(std::env::var("PROFILE").unwrap())),
